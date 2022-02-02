2c917834f9a4851a7ba1373f8c050c0d8102beca
kamilovsky <muca.kamil@gmail.com>
2022-02-02 12:10:11 +0100

    Store NFTAccess contract address in a predefined slot

--- contracts/abstract/NFTAccess.sol
+++ contracts/abstract/NFTAccess.sol
@@ -5,14 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 abstract contract NFTAccess is OwnableUpgradeable {
-    ERC721 private accessNFT;
+    bytes32 internal constant ACCESS_NFT_SLOT = bytes32(uint256(keccak256('ACCESS_NFT_SLOT')) - 1);
 
     // Setting the address to a zero address removes the access lock.
     function setAccessNFT(ERC721 nftAddress) external onlyOwner {
-        accessNFT = nftAddress;
+        bytes32 slot = ACCESS_NFT_SLOT;
+        assembly {
+            sstore(slot, nftAddress)
+        }
     }
 
     modifier hasAccessNFT {
+        bytes32 slot = ACCESS_NFT_SLOT;
+        ERC721 accessNFT;
+        assembly {
+            accessNFT := sload(slot)
+        }
         if(address(accessNFT) != address(0)) {
             require(accessNFT.balanceOf(msg.sender) > 0, "Access NFT required");
         }
