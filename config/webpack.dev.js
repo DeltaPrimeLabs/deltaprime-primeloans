2e88fc23f6dc2bee6c38b2ee95b343ecc346aa22
piotruda <piotrduda92@gmail.com>
2022-02-08 10:11:05 +0000

    Webpack fixes

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -28,5 +28,10 @@ module.exports = merge(common, {
     plugins: [
         // Only update what has changed on hot reload
         new webpack.HotModuleReplacementPlugin(),
+
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            'process.env.NODE_DEBUG': JSON.stringify('true')
+        })
     ],
 })
