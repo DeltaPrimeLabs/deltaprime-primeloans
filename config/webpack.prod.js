86907e5ec76476fc1bc45d27d47af2ce8a7ff69f
piotruda <piotrduda92@gmail.com>
2022-02-08 20:37:51 +0000

    UI: Fix selling assets

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,7 @@
 const paths = require('./paths')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const webpack = require('webpack')
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
