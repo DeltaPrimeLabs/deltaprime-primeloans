73b108c7b7fa0a40401406e68fbcc6329ec39370
piotruda <piotrduda92@gmail.com>
2022-02-11 10:03:37 +0000

    PoolFactory

---
+++ contracts/PoolFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "./Pool.sol";
+
+
+/**
+ * @title PoolFactory
+ * @dev Contract factory allowing anyone to deploy a pool contract
+ */
+contract PoolFactory {
+  function deployPool() public {
+    Pool pool = new Pool();
+    emit PoolDeployed(address(pool));
+  }
+
+  /**
+   * @dev emitted after pool is deployed by any user
+   * @param poolAddress of deployed pool
+   **/
+  event PoolDeployed(address poolAddress);
+}
--- migrations/3_lending_pool_deployment.js
+++ migrations/3_lending_pool_deployment.js
@@ -1,10 +1,16 @@
-const Pool = artifacts.require("./Pool.sol");
+const PoolFactory = artifacts.require("./PoolFactory.sol");
 const PoolTUP = artifacts.require("./PoolTUP.sol");
 
-module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(Pool);
-  console.log(`Deployed Pool implementation contract at address: ${Pool.address}`);
+module.exports = async function(deployer, accounts) {
+  await deployer.deploy(PoolFactory);
 
-  await deployer.deploy(PoolTUP, Pool.address, accounts[1], []);
-  console.log(`Deployed Pool (TransparentUpgradeableProxy). Proxy address: ${PoolTUP.address}`);
+  const factory = await PoolFactory.deployed();
+
+  const tx = await factory.deployPool();
+  let poolAddress = tx.receipt.logs[0].args[0];
+
+  console.log(`Deployed Pool implementation by factory at address: ${poolAddress}`);
+
+  await deployer.deploy(PoolTUP, poolAddress, accounts[1], []);
+  console.log(`Deployed Pool (TransparentUpgradeableProxy). Proxy address: ${poolAddress}`);
 };
