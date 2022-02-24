09c9fb4461b0dc26af4b7af50aee207381712afa
kamilovsky <muca.kamil@gmail.com>
2022-02-24 09:42:50 +0000

    Disable embedding commit hash with an env variable

--- tools/scripts/embed-commit-hash.js
+++ tools/scripts/embed-commit-hash.js
@@ -1,21 +1,25 @@
 import {execSync} from "child_process";
 
 export function embedCommitHash(contractName, contractPath = './contracts/') {
-    const fs = require('fs');
-    const path = require('path')
+    if(process.env.DISABLE_EMBEDDING_COMMIT_HASH === 'True') {
+        console.log('Skipping embedding commit hash due to the env variable "DISABLE_EMBEDDING_COMMIT_HASH" being set to "True"');
+    } else {
+        const fs = require('fs');
+        const path = require('path')
 
-    const filePath = path.join(contractPath, contractName + '.sol');
-    let data = fs.readFileSync(filePath, 'utf8');
+        const filePath = path.join(contractPath, contractName + '.sol');
+        let data = fs.readFileSync(filePath, 'utf8');
 
-    const commitHash = require('child_process')
-        .execSync('git rev-parse HEAD')
-        .toString().trim();
-    let result = data.replace(/\/\/ Last deployed using commit: .*;/g, `// Last deployed from commit: ${commitHash};`);
+        const commitHash = require('child_process')
+            .execSync('git rev-parse HEAD')
+            .toString().trim();
+        let result = data.replace(/\/\/ Last deployed using commit: .*;/g, `// Last deployed from commit: ${commitHash};`);
 
-    fs.writeFileSync(filePath, result, 'utf8');
+        fs.writeFileSync(filePath, result, 'utf8');
 
-    const output = execSync('npx hardhat compile', { encoding: 'utf-8' });
-    console.log(output);
+        const output = execSync('npx hardhat compile', { encoding: 'utf-8' });
+        console.log(output);
 
-    console.log(`Successfully baked commit ${commitHash} into the ${filePath} file.`);
+        console.log(`Successfully baked commit ${commitHash} into the ${filePath} file.`);
+    }
 }
