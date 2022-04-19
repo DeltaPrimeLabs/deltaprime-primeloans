57138f653899c514b65b22ceb481c25946bfd992
LeCh1ffre <lechiffre@deltaprime.io>
2022-04-19 15:56:53 +0200

    Nfts list UI part

--- src/components/BorrowNft.vue
+++ src/components/BorrowNft.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-    <Mint :hasNft="hasBorrowNft" :nftContract="borrowNftContract" :mintNFT="mintBorrowNft" :nftImageUri="borrowNftImageUri" :getNftId="getBorrowNftId"/>
+    <Mint v-if="showMintBox" :hasNft="hasBorrowNft" :nftContract="borrowNftContract" :mintNFT="mintBorrowNft" :nftImageUri="borrowNftImageUri" :getNftId="getBorrowNftId"/>
+    <NftList></NftList>
   </div>
 </template>
 
 <script>
 import Mint from "../components/Mint";
+import NftList from "../components/NftList";
+
 const ethers = require('ethers');
 import {mapActions, mapState} from "vuex";
 
 export default {
   name: 'BorrowNft',
   components: {
-    Mint
+    Mint,
+    NftList
   },
   computed: {
     ...mapState('network', ['provider']),
@@ -20,6 +24,14 @@ export default {
   },
   methods: {
     ...mapActions('nft', ['getBorrowNftId', 'mintBorrowNft'])
+  },
+  data() {
+    return {
+      showMintBox: true,
+    }
+  },
+  mounted() {
+    this.showMintBox = this.$route.query.signature
   }
 }
 </script>
---
+++ src/components/NftList.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="NftList" class="nft-list">
+    <div class="page-title">
+      Your NFTs
+    </div>
+
+    <div class="nfts-grid">
+      <div class="nft nft-video" v-for="nft in getNfts">
+        <video class="video" muted autoplay loop :src="nft.url"></video>
+      </div>
+      <div v-for="placeholder in numberOfPlaceholders" class="nft nft-placeholder"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+import SmallBlock from "./SmallBlock";
+
+export const LG_BRAKEPOINT = 992;
+export const SM_BRAKEPOINT = 576;
+
+export default {
+  name: 'NftList',
+  components: {
+    SmallBlock
+  },
+  data() {
+    return {
+      numberOfPlaceholders: 3,
+    }
+  },
+  computed: {
+    getNfts() {
+      const nfts = [
+        {
+          url: 'https://arweave.net/Ga5-ypvTdKRG2YUYIxWjVDlgSzJgWgbeMIoge6kpeOs'
+        },
+        {
+          url: 'https://arweave.net/Ga5-ypvTdKRG2YUYIxWjVDlgSzJgWgbeMIoge6kpeOs'
+        },
+      ];
+      this.setupNumberOfPlaceholders(nfts.length);
+      return nfts
+    },
+  },
+  methods: {
+    setupNumberOfPlaceholders(numberOfNfts) {
+      let innerWidth = window.innerWidth;
+      if (innerWidth > LG_BRAKEPOINT) {
+        this.numberOfPlaceholders = 3 - numberOfNfts;
+      } else if (innerWidth < LG_BRAKEPOINT && innerWidth > SM_BRAKEPOINT) {
+        this.numberOfPlaceholders = 2 - numberOfNfts;
+      } else {
+        this.numberOfPlaceholders = 0;
+      }
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import "~@/styles/variables";
+
+.nft-list {
+  max-width: 970px;
+  margin: auto;
+
+  @media screen and (max-width: $lg) {
+    padding: 0 20px;
+  }
+}
+
+.page-title {
+  font-size: 24px;
+  font-weight: bold;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 40px;
+}
+
+.nfts-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 40px;
+  row-gap: 40px;
+
+  @media screen and (max-width: $lg) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: $sm) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+
+  .nft {
+    width: 100%;
+    height: 450px;
+    border-radius: 23px;
+  }
+
+  .video {
+    width: 100%;
+    height: 450px;
+    object-fit: cover;
+    border-radius: 23px;
+  }
+
+  .nft-placeholder {
+    opacity: 0.08;
+    background-image: linear-gradient(152deg, #7476fc 23%, #ff6f43 65%, #f5217f 96%);
+  }
+}
+</style>
