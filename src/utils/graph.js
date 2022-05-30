b79f3cb68d8a57edc5ff2442315f4701b7dadfa2
mbare0 <mbare@deltaprime.io>
2022-05-30 10:15:42 +0200

    Review: Fetching events from subgraph

---
+++ src/utils/graph.js
@@ -0,0 +1,78 @@
+import config from "@/config";
+import ApolloClient, {gql} from "apollo-boost";
+import {fromWei} from "./calculate";
+
+
+export async function fetchEventsForSmartLoan(address) {
+    let query = `
+    {
+      smartLoanEvents(where: { smartLoan: "${address.toString()}"}, orderBy: timestamp) {
+        id
+        name
+        toAsset
+        amount
+        timestamp
+      }
+    }
+   `;
+
+    const client = new ApolloClient({
+        uri: config.subgraph
+    })
+
+    return (await client.query({query: gql(query)})).data.smartLoanEvents;
+}
+
+export async function fetchCollateralFromPayments(address) {
+    let query = `
+    {   
+        smartLoan(id: "${address}") {
+            collateralFromPayments
+        }
+    }
+   `;
+
+    const client = new ApolloClient({
+        uri: config.subgraph
+    })
+
+    return fromWei((await client.query({query: gql(query)})).data.smartLoan.collateralFromPayments);
+}
+
+export async function fetchEventsForPool(address) {
+    let query = `
+    {
+      poolEvents(where: { user: "${address.toString()}"}, orderBy: timestamp) {
+        id
+        name
+        amount
+        timestamp
+      }
+    }
+   `;
+
+    const client = new ApolloClient({
+        uri: config.subgraph
+    })
+
+    return (await client.query({query: gql(query)})).data.poolEvents;
+}
+
+export async function fetchDepositFromPayments(address) {
+    let query = `
+    {   
+        poolUser(id: "${address}") {
+            depositFromPayments
+        }
+    }
+   `;
+
+    const client = new ApolloClient({
+        uri: config.subgraph
+    })
+
+    let user = (await client.query({query: gql(query)})).data.poolUser;
+    if (user) {
+        return fromWei((await client.query({query: gql(query)})).data.poolUser.depositFromPayments);
+    } else return 0;
+}
