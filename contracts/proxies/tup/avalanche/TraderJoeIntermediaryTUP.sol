a92375ad270beb4df38872d6df31112bd7da698f
mbare0 <mbare@deltaprime.io>
2022-09-10 16:13:24 +0200

    Add TJ TUP and update token addresses

---
+++ contracts/proxies/tup/avalanche/TraderJoeIntermediaryTUP.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BUSL-1.1
+// Last deployed from commit: 43879c9dc673fbc3c0974b8106a295ba5c5e6774;
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract TraderJoeIntermediaryTUP is TransparentUpgradeableProxy {
+    constructor(address _logic, address admin_, bytes memory _data) TransparentUpgradeableProxy(_logic, admin_, _data) {}
+}
