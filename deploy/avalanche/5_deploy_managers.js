fbb9691789b2257562ab9683df900beb65e0b43d
mbare0 <mbare@deltaprime.io>
2022-09-14 10:27:38 +0200

    Update signers list

--- deploy/avalanche/5_deploy_managers.js
+++ deploy/avalanche/5_deploy_managers.js
@@ -59,22 +59,36 @@ module.exports = async ({
 
     console.log(`Deployed tokenManager at address: ${tokenManager.address}`);
 
-    //TODO: update signers
+    //TODO: check before the production deploy
     let redstoneConfigManager = await deploy('RedstoneConfigManager', {
         from: deployer,
         gasLimit: 8000000,
         args:
         [
             [
-                "0xFE71e9691B9524BC932C23d0EeD5c9CE41161884",
-                "0x1cd8f9627a2838a7dae6b98cf71c08b9cbf5174a",
-                "0x981bda8276ae93f567922497153de7a5683708d3",
-                "0x3befdd935b50f172e696a5187dbacfef0d208e48",
-                "0xc1d5b940659e57b7bdf8870cdfc43f41ca699460",
-                "0xbc5a06815ee80de7d20071703c1f1b8fc511c7d4",
-                "0x496f4e8ac11076350a59b88d2ad62bc20d410ea3",
-                "0xe9fa2869c5f6fc3a0933981825564fd90573a86d",
-                "0xdf6b1ca313bee470d0142279791fa760abf5c537",
+                "0x981bdA8276ae93F567922497153de7A5683708d3",
+                "0x3BEFDd935b50F172e696A5187DBaCfEf0D208e48",
+                "0x1Cd8F9627a2838a7DAE6b98CF71c08B9CbF5174a",
+                "0xbC5a06815ee80dE7d20071703C1F1B8fC511c7d4",
+                "0xe9Fa2869C5f6fC3A0933981825564FD90573A86D",
+                "0xDf6b1cA313beE470D0142279791Fa760ABF5C537",
+                "0xa50abc5D76dAb99d5fe59FD32f239Bd37d55025f",
+                "0x496f4E8aC11076350A59b88D2ad62bc20d410EA3",
+                "0x41FB6b8d0f586E73d575bC57CFD29142B3214A47",
+                "0xC1068312a6333e6601f937c4773065B70D38A5bF",
+                "0xAE9D49Ea64DF38B9fcbC238bc7004a1421f7eeE8",
+                "0x2BC37a0368E86cA0d14Bc8788D45c75deabaC064",
+                "0x9277491f485460575918B43f5d6D5b2BB8c5A62d",
+                "0x91dC1fe6472e18Fd2C9407e438dD022f22891a4f",
+                "0x4bbb86992E94AA209c52ecfd38897A18bde8E39D",
+                "0x9456dd79c3608cF463d975F76f7658f87a41Cd6C",
+                "0x4C6f83Faa74106139FcB08d4E49568e0Df222815",
+                "0x4CF8310ABAe9CA2ACD85f460B509eE495F36eFAF",
+                "0x2D0645D863a4eE15664761ea1d99fF2bae8aAe35",
+                "0xF5c14165fb10Ac4926d52504a9B45550411A3C0F",
+                "0x11D23F3dbf8B8e1cf61AeF77A2ea0592Bc9860E0",
+                "0x60930D9f74811B525356E68D23977baEAb7706d0",
+                "0xc1D5b940659e57b7bDF8870CDfC43f41Ca699460"
             ]
         ]
     });
