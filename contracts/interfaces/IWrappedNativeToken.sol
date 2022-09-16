79a135aac7acef19dd9d65f12bd814942cca1a70
mbare0 <mbare@deltaprime.io>
2022-09-16 19:22:22 +0200

    TASK_#144

--- contracts/interfaces/IWrappedNativeToken.sol
+++ contracts/interfaces/IWrappedNativeToken.sol
@@ -2,14 +2,20 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+interface IWrappedNativeToken {
 
-interface IWrappedNativeToken is IERC20 {
+    function balanceOf(address account) external view returns (uint);
 
-function receive() external payable;
+    function deposit() external payable;
 
-function deposit() external payable;
+    function withdraw(uint wad) external;
 
-function withdraw(uint256 wad) external;
+    function totalSupply() external view returns (uint);
+
+    function approve(address guy, uint wad) external returns (bool);
+
+    function transfer(address dst, uint wad) external returns (bool);
+
+    function transferFrom(address src, address dst, uint wad) external returns (bool);
 
 }
