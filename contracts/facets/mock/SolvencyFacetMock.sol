25a6e3a8e99d55430638a801247bf1f5d03f619c
kamilovsky <muca.kamil@gmail.com>
2022-11-30 16:17:52 +0100

    T-017

--- contracts/facets/mock/SolvencyFacetMock.sol
+++ contracts/facets/mock/SolvencyFacetMock.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "redstone-evm-connector/lib/contracts/message-based/PriceAware.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../../oracle/RSOracleMock3Signers.sol";
 import "../../TokenManager.sol";
