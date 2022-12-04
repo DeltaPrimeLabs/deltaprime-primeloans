87718fafe8530eb1f5917923bd299a7000a00d0d
mbare0 <mbare@deltaprime.io>
2022-12-04 19:15:02 +0100

    Do not deploy Beefy facet

--- test/_helpers.ts
+++ test/_helpers.ts
@@ -518,7 +518,7 @@ export const deployAllFacets = async function (diamondAddress: any, mock: boolea
             'unstakeTJAVAXSAVAXYak'
 
         ], hardhatConfig)
-        await deployFacet("BeefyFinanceAvalancheFacet", diamondAddress, ['stakePngUsdcAvaxLpBeefy', 'stakePngUsdceAvaxLpBeefy' ,'stakeTjUsdcAvaxLpBeefy', 'unstakePngUsdcAvaxLpBeefy', 'unstakePngUsdceAvaxLpBeefy', 'unstakeTjUsdcAvaxLpBeefy'], hardhatConfig)
+        // await deployFacet("BeefyFinanceAvalancheFacet", diamondAddress, ['stakePngUsdcAvaxLpBeefy', 'stakePngUsdceAvaxLpBeefy' ,'stakeTjUsdcAvaxLpBeefy', 'unstakePngUsdcAvaxLpBeefy', 'unstakePngUsdceAvaxLpBeefy', 'unstakeTjUsdcAvaxLpBeefy'], hardhatConfig)
         await deployFacet("VectorFinanceFacet", diamondAddress, [
                 'vectorStakeUSDC1',
                 'vectorUnstakeUSDC1',
