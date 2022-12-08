f40fdef739ceea05bb5598587f282ef6ce2b1089
mbare0 <mbare@deltaprime.io>
2022-12-08 21:45:27 +0100

    Update IYieldYakFacet methods

--- contracts/interfaces/facets/avalanche/IYieldYakFacet.sol
+++ contracts/interfaces/facets/avalanche/IYieldYakFacet.sol
@@ -5,11 +5,27 @@ interface IYieldYakFacet {
 
     function stakeSAVAXYak(uint256 amount) external;
 
+    function stakePNGAVAXUSDCYak(uint256 amount) external;
+
+    function stakePNGAVAXETHYak(uint256 amount) external;
+
+    function stakeTJAVAXETHYak(uint256 amount) external;
+
     function stakeTJAVAXUSDCYak(uint256 amount) external;
+
+    function stakeTJAVAXSAVAXYak(uint256 amount) external;
 
     function unstakeAVAXYak(uint256 amount) external;
 
     function unstakeSAVAXYak(uint256 amount) external;
 
+    function unstakePNGAVAXUSDCYak(uint256 amount) external;
+
+    function unstakePNGAVAXETHYak(uint256 amount) external;
+
+    function unstakeTJAVAXETHYak(uint256 amount) external;
+
     function unstakeTJAVAXUSDCYak(uint256 amount) external;
+
+    function unstakeTJAVAXSAVAXYak(uint256 amount) external;
 }
