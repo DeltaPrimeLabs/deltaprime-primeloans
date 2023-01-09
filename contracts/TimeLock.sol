9bfc30978a33c1655fe230b363c92e6e81e12fd9
kamilovsky <muca.kamil@gmail.com>
2023-01-09 18:44:28 +0100

    Add deployment jsons

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -13,8 +13,8 @@ contract Timelock {
     event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature,  bytes data, uint eta);
     event QueueTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature, bytes data, uint eta);
 
-    uint public constant GRACE_PERIOD = 14 days;
-    uint public constant MINIMUM_DELAY = 10 minutes;
+    uint public constant GRACE_PERIOD = 7 days;
+    uint public constant MINIMUM_DELAY = 1 days;
     uint public constant MAXIMUM_DELAY = 30 days;
 
     address public admin;
