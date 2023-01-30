4feac03197db290885a4c4581284336692077e94
mbare0 <mbare@deltaprime.io>
2023-01-30 16:56:34 +0100

    Update events for operations with native token

--- contracts/interfaces/facets/ISmartLoanWrappedNativeTokenFacet.sol
+++ contracts/interfaces/facets/ISmartLoanWrappedNativeTokenFacet.sol
@@ -6,4 +6,10 @@ interface ISmartLoanWrappedNativeTokenFacet {
     function unwrapAndWithdraw(uint256 _amount) payable external;
 
     function wrapNativeToken(uint256 amount) external;
+
+    event WrapNative(address indexed user, uint256 amount, uint256 timestamp);
+
+    event DepositNative(address indexed user, uint256 amount, uint256 timestamp);
+
+    event UnwrapAndWithdraw(address indexed user, uint256 amount, uint256 timestamp);
 }
