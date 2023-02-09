8b9339004f4590eda98d300469d93a96454164e4
mbare0 <mbare@deltaprime.io>
2023-02-09 12:45:25 +0100

    UI: update GLP banner

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
       The protocol detected unusual market behavior. Some functions might be not available.
     </Banner>
     <Banner v-if="showGlpBanner" background="green">
-      GLP integration is ready! Mint or deposit GLP and stake it on the Farms page.
+      GLP integration is ready! Mint or deposit GLP and stake it on the Farms page to get your Account APY back up.
     </Banner>
     <div class="content">
       <div class="top-bar">
