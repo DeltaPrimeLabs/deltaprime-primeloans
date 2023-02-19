434e25d17f91ec98e8ee6740f4d5613bc9095a24
mbare0 <mbare@deltaprime.io>
2023-02-19 17:04:36 +0100

    BTC and ETH pools deployment files

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: a8429e3dd31e4164503af56863fbb4a7e868ceb6;
+// Last deployed from commit: 2f6b0fb53889a8741a3d7f78a2d5d05ad7a0c76d;
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
--- contracts/deployment/avalanche/WavaxVariableUtilisationRatesCalculator.sol
+++ contracts/deployment/avalanche/WavaxVariableUtilisationRatesCalculator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: 01a6c6606673961fa6183b54b8b35220d47e99fc;
+// Last deployed from commit: 2f6b0fb53889a8741a3d7f78a2d5d05ad7a0c76d;
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
