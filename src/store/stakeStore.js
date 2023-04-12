9c40b9b9b4d520e42706ba7a96e51405c9e4e9ad
LeCh1ffre <lechiffre@deltaprime.io>
2023-04-12 15:21:19 +0200

    UI: fix 0 health bug

--- src/store/stakeStore.js
+++ src/store/stakeStore.js
@@ -331,6 +331,7 @@ export default {
         farmService.emitRefreshFarm();
         commit('setFarms', farms);
         farmService.emitRefreshFarm();
+        rootState.serviceRegistry.healthService.emitRefreshHealth();
       })
 
 
