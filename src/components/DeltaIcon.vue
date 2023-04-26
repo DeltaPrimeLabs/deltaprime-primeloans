f53f1f4631be5f46ecc6992b81018300d8976560
LeCh1ffre <lechiffre@deltaprime.io>
2023-04-26 14:59:18 +0200

    UI: transaction history disable hover on swap icon

--- src/components/DeltaIcon.vue
+++ src/components/DeltaIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="icon" class="delta-icon" v-bind:style="`height: ${$props.size}px; width: ${$props.size}px`"></div>
+  <div ref="icon" class="delta-icon" v-bind:class="{'delta-icon--no-hover': noHover}" v-bind:style="`height: ${$props.size}px; width: ${$props.size}px`"></div>
 </template>
 
 <script>
@@ -14,6 +14,10 @@ export default {
       type: String,
       default: ''
     },
+    noHover: {
+      type: Boolean,
+      default: false,
+    }
   },
   mounted() {
     this.$refs.icon.style.webkitMaskImage = `url(${this.iconSrc})`;
@@ -32,5 +36,9 @@ export default {
   mask-repeat: no-repeat;
   -webkit-mask-position: center;
   mask-position: center;
+
+  &.delta-icon--no-hover {
+    pointer-events: none;
+  }
 }
 </style>
