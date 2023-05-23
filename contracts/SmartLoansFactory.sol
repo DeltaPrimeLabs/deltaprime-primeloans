7d2c098fe8374aa7a6c8b9104a70b8c86ab6be34
0xlock <0xl0ck@proton.me>
2023-05-23 10:00:58 -0500

    deploy new SmartLoansFactory implementation

--- contracts/SmartLoansFactory.sol
+++ contracts/SmartLoansFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: 8c36e18a206b9e6649c00da51c54b92171ce3413;
+// Last deployed from commit: afa1ac0e9852d3fd53b049f6492a26e0a7d279dc;
 pragma solidity 0.8.17;
 
 import "@redstone-finance/evm-connector/contracts/core/ProxyConnector.sol";
