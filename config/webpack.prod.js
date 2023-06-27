ed12e7ae867cb6432e1546aafee0094ac0240f19
keizir <ultimatesofteng.work@gmail.com>
2023-06-28 00:18:39 +0800

    UI: add sass loader to webpack prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -34,8 +34,13 @@ module.exports = merge(common, {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // fallback to style-loader in development
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
             },
         ],
     },
