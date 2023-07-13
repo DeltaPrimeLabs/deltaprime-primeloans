44cec004c6340de2fe3f85d3db928583310632d3
LeCh1ffre <lechiffre@deltaprime.io>
2023-07-13 15:47:01 +0200

    UI: add USDC platypus banner

--- src/App.vue
+++ src/App.vue
@@ -39,9 +39,12 @@
       </a>
     </Banner>
     <Banner v-if="showPrimeAccountBanner" :closable="true">
-      Due to the Platypus exploit, data coming from Platypus might be incorrect. We are working on solving this asap.
-      <a class="banner-link" href="https://discord.com/channels/889510301421166643/912702114252329060" target="_blank"><b>
-        Read more</b></a>.
+      Important message to Platypus USDC farmers before interacting with the pool:
+      <a class="banner-link" href="https://discord.com/channels/889510301421166643/912702114252329060/1129034316941377616" target="_blank">
+        <b>
+          Read More.
+        </b>
+      </a>
     </Banner>
     <div class="content">
       <div class="top-bar">
