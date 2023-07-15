405a364be9223e91f6c1839b2a2240354b70f2db
kavsky02 <kamilovsky@deltaprime.io>
2023-07-15 17:34:24 +0200

    Refactor Convex facet

--- contracts/interfaces/arbitrum/IConvexPool.sol
+++ contracts/interfaces/arbitrum/IConvexPool.sol
@@ -3,4 +3,22 @@ pragma solidity 0.8.17;
 
 interface IConvexPool {
     function deposit(uint256 _pid, uint256 _amount) external returns(bool);
+
+    struct DepositDetails {
+        uint256 crvLpTokenAmount;
+        uint256 depositPoolId;
+        address depositPoolAddress;
+        address crvLpTokenAddress;
+        bytes32 crvLpTokenIdentifier;
+        address cvxPoolLPTokenAddress;
+        bytes32 cvxPoolIdentifier;
+    }
+
+    struct WithdrawalDetails {
+        uint256 receiptTokenAmount;
+        address crvLpTokenAddress;
+        bytes32 crvLpTokenIdentifier;
+        address cvxPoolLPTokenAddress;
+        bytes32 cvxPoolIdentifier;
+    }
 }
