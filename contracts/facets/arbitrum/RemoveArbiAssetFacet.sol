20f2b91bd9dfe7424173ae665bc19310ec37d9dd
kavsky02 <kamilovsky@deltaprime.io>
2023-08-30 15:35:01 +0200

    Remove ARBI asset from storage fix v2

--- contracts/facets/arbitrum/RemoveArbiAssetFacet.sol
+++ contracts/facets/arbitrum/RemoveArbiAssetFacet.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: 64aac14edbacb47ae29adb98d3b856a351895646;
+// Last deployed from commit: b008d1cca9fa95e6f83e76e6ee4dcf958513a113;
 pragma solidity 0.8.17;
 
 import {DiamondStorageLib} from "../../lib/DiamondStorageLib.sol";
 
 contract RemoveArbiAssetFacet  {
     function removeArbiAsset() external {
-        require(msg.sender == 0xDB246e0fc9029fFBFE93F60399Edcf3cf279901c, "Wrong msg.sender");
+        require(msg.sender == 0x5D80a1c0a5084163F1D2620c1B1F43209cd4dB12, "Wrong msg.sender");
         DiamondStorageLib.removeOwnedAsset("ARBI");
     }
 }
