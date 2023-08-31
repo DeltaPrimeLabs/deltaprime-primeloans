84a6e22fc76927608000c1aa12297e3e4b584f16
LeCh1ffre <lechiffre@deltaprime.io>
2023-08-31 12:35:18 +0200

    UI: fix toggle component

--- src/components/NetworkSelect.vue
+++ src/components/NetworkSelect.vue
@@ -130,108 +130,108 @@ export default {
 <style scoped>
 .network-select {
   position: relative;
-}
 
-.indicator {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 6px;
-  cursor: pointer;
-}
-
-.chevron {
-  background: var(--network-select__chevron-color);
-  transition: transform 200ms ease-in-out;
-
-  &.chevron--open {
-    transform: rotate(180deg);
-  }
-}
-
-.dropdown__wrapper {
-  position: absolute;
-  top: calc(100% + 9px);
-  left: 50%;
-  transform: translateX(-50%);
-  overflow: hidden;
-  z-index: 1;
-}
-
-.dropdown {
-  padding: 10px 12px;
-  border-radius: 10px;
-  border: 2px solid var(--network-select__dropdown-border-color);
-  background: var(--network-select__dropdown-background);
-  box-shadow: var(--network-select__dropdown-shadow);
-}
-
-.dropdown__header {
-  font-size: 12px;
-  color: var(--network-select__dropdown-header-color);
-  margin-bottom: 9px;
-  margin-left: 11px;
-  pointer-events: none;
-}
-
-.option {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 132px;
-  cursor: pointer;
-  padding: 6px 10px;
-  border-radius: 5px;
-  color: var(--network-select__option-name-color);
-  border: 1px solid transparent;
-
-  &:not(:last-child) {
-    margin-bottom: 6px;
+  .indicator {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 6px;
+    cursor: pointer;
   }
 
-  &.option--selected {
-    cursor: auto;
-    background: var(--network-select__option-background--selected);
-    font-weight: 600;
-    pointer-events: none;
+  .chevron {
+    background: var(--network-select__chevron-color);
+    transition: transform 200ms ease-in-out;
 
-    .option__name {
-      color: var(--network-select__option-name-color--selected);
+    &.chevron--open {
+      transform: rotate(180deg);
     }
   }
 
-  &:not(.option--selected):hover {
-    border-color: var(--network-select__option-border--hover);
+  .dropdown__wrapper {
+    position: absolute;
+    top: calc(100% + 9px);
+    left: 50%;
+    transform: translateX(-50%);
+    overflow: hidden;
+    z-index: 1;
   }
-}
 
-.option__logo {
-  height: 18px;
-  width: 18px;
-  margin-right: 8px;
-}
+  .dropdown {
+    padding: 10px 12px;
+    border-radius: 10px;
+    border: 2px solid var(--network-select__dropdown-border-color);
+    background: var(--network-select__dropdown-background);
+    box-shadow: var(--network-select__dropdown-shadow);
+  }
 
-.logo {
-  height: 18px;
-  width: 18px;
-}
+  .dropdown__header {
+    font-size: 12px;
+    color: var(--network-select__dropdown-header-color);
+    margin-bottom: 9px;
+    margin-left: 11px;
+    pointer-events: none;
+  }
 
-.network {
-  font-size: 16px;
-  color: var(--network-select__indicator-network-color);
-}
+  .option {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 132px;
+    cursor: pointer;
+    padding: 6px 10px;
+    border-radius: 5px;
+    color: var(--network-select__option-name-color);
+    border: 1px solid transparent;
 
-.slide-enter-active,
-.slide-leave-active {
-  transition: transform 0.25s ease;
-  transform: translateY(-100%);
-}
+    &:not(:last-child) {
+      margin-bottom: 6px;
+    }
 
-.slide-leave-to {
-  transform: translateY(-100%);
-}
+    &.option--selected {
+      cursor: auto;
+      background: var(--network-select__option-background--selected);
+      font-weight: 600;
+      pointer-events: none;
 
-.slide-enter-to {
-  transform: translateY(0%);
+      .option__name {
+        color: var(--network-select__option-name-color--selected);
+      }
+    }
+
+    &:not(.option--selected):hover {
+      border-color: var(--network-select__option-border--hover);
+    }
+  }
+
+  .option__logo {
+    height: 18px;
+    width: 18px;
+    margin-right: 8px;
+  }
+
+  .logo {
+    height: 18px;
+    width: 18px;
+  }
+
+  .network {
+    font-size: 16px;
+    color: var(--network-select__indicator-network-color);
+  }
+
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: transform 0.25s ease;
+    transform: translateY(-100%);
+  }
+
+  .slide-leave-to {
+    transform: translateY(-100%);
+  }
+
+  .slide-enter-to {
+    transform: translateY(0%);
+  }
 }
 </style>
