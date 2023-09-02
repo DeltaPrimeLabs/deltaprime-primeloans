fb2bed7fc97896b3fb524b95c6a9c10fd87742a9
mbare <mbare@deltaprime.io>
2023-09-02 22:05:54 +0200

    UI: Revert call contract script changes

--- tools/scripts/call-contract.js
+++ tools/scripts/call-contract.js
@@ -1,7 +1,9 @@
-const contractAddress = "0xD446eb1660F766d533BeCeEf890Df7A69d26f7d1";
-const contractMethod = "getPriceFromId";
-const jsonRPC = "https://api.avax.network/ext/bc/C/rpc";
+const contractName = "contract_name";
+const contractAddress = "contract_address";
+const contractMethod = "method_name";
+const jsonRPC = "https_address";
 
+const ARTIFACT = require(`../../artifacts/contracts/${contractName}.sol/${contractName}.json`);
 const ethers = require("ethers");
 const fs = require("fs");
 
@@ -10,17 +12,9 @@ let mnemonicWallet = new ethers.Wallet(key);
 let provider = new ethers.providers.JsonRpcProvider(jsonRPC);
 let wallet = mnemonicWallet.connect(provider);
 
-const LBTokenAbi = [
-    'function balanceOf(address account, uint256 id) external view returns (uint256)',
-    'function getActiveId() external view returns (uint24)',
-    'function getPriceFromId(uint24 id) external view returns (uint256 price)',
-    'function name() external view returns (string memory)',
-    'function approveForAll(address spender, bool approved) external'
-]
+let contract = new ethers.Contract(contractAddress, ARTIFACT.abi, wallet);
 
-let contract = new ethers.Contract(contractAddress, LBTokenAbi, wallet);
-
-contract[contractMethod](10).then(
+contract[contractMethod]().then(
     res => {
         console.log(res);
     }
