0567ff2fbc691703a896e44f304d3ea072803b61
kavsky02 <kamilovsky@deltaprime.io>
2023-09-04 12:50:07 +0200

    Redeploy TraderJoeV2ArbitrumFacet.json

--- deploy/arbitrum/19_deploy_usdc_eth_pools_impl.js
+++ deploy/arbitrum/19_deploy_usdc_eth_pools_impl.js
@@ -11,7 +11,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     embedCommitHash("Pool", "./contracts");
 
     embedCommitHash("EthPool", "./contracts/deployment/arbitrum");
-    // embedCommitHash("UsdcPool", "./contracts/deployment/arbitrum");
+    embedCommitHash("UsdcPool", "./contracts/deployment/arbitrum");
 
     const ethPool = await deploy("WethPool", {
         contract: "contracts/deployment/arbitrum/WethPool.sol:WethPool",
@@ -32,30 +32,30 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         });
     console.log(`Verified WethPool`);
 
-    // const usdcPool = await deploy("UsdcPool", {
-    //     contract: "contracts/deployment/arbitrum/UsdcPool.sol:UsdcPool",
-    //     from: deployer,
-    //     gasLimit: 20000000,
-    //     args: [],
-    // });
-    //
-    // console.log(
-    //     `Deployed UsdcPool at address: ${usdcPool.address}`
-    // );
-    //
-    // await verifyContract(hre,
-    //     {
-    //         address: usdcPool.address,
-    //         contract: `contracts/deployment/arbitrum/UsdcPool.sol:UsdcPool`,
-    //         constructorArguments: []
-    //     });
-    // console.log(`Verified UsdcPool`);
+    const usdcPool = await deploy("UsdcPool", {
+        contract: "contracts/deployment/arbitrum/UsdcPool.sol:UsdcPool",
+        from: deployer,
+        gasLimit: 20000000,
+        args: [],
+    });
+
+    console.log(
+        `Deployed UsdcPool at address: ${usdcPool.address}`
+    );
+
+    await verifyContract(hre,
+        {
+            address: usdcPool.address,
+            contract: `contracts/deployment/arbitrum/UsdcPool.sol:UsdcPool`,
+            constructorArguments: []
+        });
+    console.log(`Verified UsdcPool`);
 
     const ethPoolTUP = await ethers.getContract("WethPoolTUP", admin);
-    // const usdcPoolTUP = await ethers.getContract("UsdcPoolTUP", admin);
+    const usdcPoolTUP = await ethers.getContract("UsdcPoolTUP", admin);
 
     await ethPoolTUP.upgradeTo(ethPool.address);
-    // await usdcPoolTUP.upgradeTo("0xEaf5a4259D8c9828c46Eb87b4801D7caDcEF340f");
+    await usdcPoolTUP.upgradeTo("0xEaf5a4259D8c9828c46Eb87b4801D7caDcEF340f");
 };
 
 
---
+++ deploy/arbitrum/20_tj_v2_arbitrum_facet_deploy.js
@@ -0,0 +1,33 @@
+import { embedCommitHash } from "../../tools/scripts/embed-commit-hash";
+
+const { ethers } = require("hardhat");
+import verifyContract from "../../tools/scripts/verify-contract";
+import hre from "hardhat";
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy } = deployments;
+    const { deployer, admin } = await getNamedAccounts();
+
+    embedCommitHash("TraderJoeV2ArbitrumFacet", "./contracts/facets/arbitrum");
+
+    let TraderJoeV2ArbitrumFacet = await deploy("TraderJoeV2ArbitrumFacet", {
+        from: deployer,
+        gasLimit: 50000000,
+        args: [],
+    });
+
+
+    console.log(
+        `TraderJoeV2ArbitrumFacet implementation deployed at address: ${TraderJoeV2ArbitrumFacet.address}`
+    );
+
+    await verifyContract(hre,
+        {
+            address: TraderJoeV2ArbitrumFacet.address,
+            contract: `contracts/facets/arbitrum/TraderJoeV2ArbitrumFacet.sol:TraderJoeV2ArbitrumFacet`,
+            constructorArguments: []
+        });
+    console.log(`Verified TraderJoeV2ArbitrumFacet`);
+};
+
+module.exports.tags = ["arbitrum-tjv2"];
