1dc9835a8d048f4d31d850d26d5539208a0cd68a
Nidhock <nidhock@op.pl>
2023-09-19 20:55:36 +0200

    UI: depositing collateral validation fix

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -26,8 +26,10 @@ export function calculateHealth(tokens, lbTokens) {
   let weightedCollateralFromLBs = 0;
 
   if (lbTokens) {
-    for (let lbToken of lbTokens) {
-      lbToken.binIds.forEach(
+    lbTokens
+      .filter(lbToken => lbToken.binIds)
+      .forEach(lbToken => {
+        lbToken.binIds.forEach(
           (binId, i) => {
             let balancePrimary = parseFloat(lbToken.binBalancePrimary[i]);
             let balanceSecondary = parseFloat(lbToken.binBalanceSecondary[i])
@@ -36,12 +38,12 @@ export function calculateHealth(tokens, lbTokens) {
             let debtCoverageSecondary = config.ASSETS_CONFIG[lbToken.secondary].debtCoverage;
 
             weightedCollateralFromLBs += Math.min(
-                debtCoveragePrimary * liquidity * config.ASSETS_CONFIG[lbToken.primary].price / lbToken.binPrices[i],
-                debtCoverageSecondary * liquidity * config.ASSETS_CONFIG[lbToken.secondary].price
+              debtCoveragePrimary * liquidity * config.ASSETS_CONFIG[lbToken.primary].price / lbToken.binPrices[i],
+              debtCoverageSecondary * liquidity * config.ASSETS_CONFIG[lbToken.secondary].price
             ) * lbToken.accountBalances[i] / lbToken.binTotalSupply[i];
           }
-      );
-    }
+        );
+      })
   }
 
   let weightedCollateral = weightedCollateralFromLBs + tokens.reduce((acc, token) => acc + token.price * (token.balance - token.borrowed) * token.debtCoverage, 0);
