67471c167ea4dcee4590ca5d8289a47373be90e3
kavsky02 <kamilovsky@deltaprime.io>
2023-09-23 17:05:33 +0200

    Add TjV2 avalanche deployment files

--- deploy/avalanche/13_tj_v2_avalanche_facet_deploy.js
+++ deploy/avalanche/13_tj_v2_avalanche_facet_deploy.js
@@ -8,9 +8,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer, admin } = await getNamedAccounts();
 
-    embedCommitHash("TraderJoeV2Facet", "./contracts/facets/avalanche");
+    embedCommitHash("TraderJoeV2AvalancheFacet", "./contracts/facets/avalanche");
 
-    let TraderJoeV2Facet = await deploy("TraderJoeV2Facet", {
+    let TraderJoeV2AvalancheFacet = await deploy("TraderJoeV2AvalancheFacet", {
         from: deployer,
         gasLimit: 15000000,
         args: [],
@@ -18,16 +18,16 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
 
     console.log(
-        `TraderJoeV2Facet implementation deployed at address: ${TraderJoeV2Facet.address}`
+        `TraderJoeV2AvalancheFacet implementation deployed at address: ${TraderJoeV2AvalancheFacet.address}`
     );
 
     await verifyContract(hre,
         {
-            address: TraderJoeV2Facet.address,
-            contract: `contracts/facets/avalanche/TraderJoeV2Facet.sol:TraderJoeV2Facet`,
+            address: TraderJoeV2AvalancheFacet.address,
+            contract: `contracts/facets/avalanche/TraderJoeV2AvalancheFacet.sol:TraderJoeV2AvalancheFacet`,
             constructorArguments: []
         });
-    console.log(`Verified TraderJoeV2Facet`);
+    console.log(`Verified TraderJoeV2AvalancheFacet`);
 };
 
 module.exports.tags = ["avalanche-tjv2"];
