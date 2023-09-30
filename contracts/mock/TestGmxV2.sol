0cd8727afe690e86c1c4a4c1a69f8227704cb95b
mbare <mbare@deltaprime.io>
2023-09-30 14:54:42 +0200

    Fix GMX addresses

--- contracts/mock/TestGmxV2.sol
+++ contracts/mock/TestGmxV2.sol
@@ -3,25 +3,27 @@ pragma solidity ^0.8.0;
 
 import "../interfaces/gmx-v2/IOrderCallbackReceiver.sol";
 import "../interfaces/gmx-v2/Order.sol";
+import "../interfaces/gmx-v2/BasicMulticall.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract TestGmxV2 is IOrderCallbackReceiver {
 
-    address GMX_V2_EXCHANGE_ROUTER_TOKEN = 0x7C68C7866A64FA2160F78EEaE12217FFbf871fa8;
+    address GMX_V2_ROUTER = 0x7452c558d45f8afC8c83dAe62C3f8A5BE19c71f6;
     address GMX_V2_DEPOSIT_VAULT = 0x2069309427e27b018222f9D298371358959EFFB3;
     address GM_ETH_USDC = 0x70d95587d40a2caf56bd97485ab3eec10bee6336;
     address ETH = 0x82af49447d8a07e3bd95bd0d56f35241523fbab1;
-    address USDC = 0xaf88d065e77c8cc2239327c5edb3a432268e5831;
+    address USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
 
     function getSelector(string calldata _func) internal pure returns (bytes4) {
-        return bytes4(keccak256(bytes_func));
+        return bytes4(keccak256(bytes(_func)));
     }
 
-    function depositEthUsdc(bool isLongToken, uint256 tokenAmount, uint256 minGmAmount, uint256 executionFee) public returns (bool) {
+    function depositEthUsdc(bool isLongToken, uint256 tokenAmount, uint256 minGmAmount, uint256 executionFee) public returns (bytes[] memory) {
         address depositedToken = isLongToken ? ETH : USDC;
 
-        IERC20(depositedToken).approve(GMX_V2_DEPOSIT_VAULT, tokenAmount);
+        IERC20(depositedToken).approve(GMX_V2_ROUTER, tokenAmount);
 
-        bytes memory callData = abi.encodePacked(
+        bytes[] memory data = [
             abi.encodeWithSelector(
                 getSelector("sendTokens"),
                 depositedToken,
@@ -43,26 +45,22 @@ contract TestGmxV2 is IOrderCallbackReceiver {
                 0, //callbackGasLimit
                 address(0) //uiFeeReceiver
             )
-        );
+        ];
 
-        (bool success, bytes memory returnData) = target.call(callData);
+        bytes[] memory results = BasicMulticall(GMX_V2_ROUTER).multicall(data);
 
         //TODO: pause the Prime Account
-        return success;
+        return results;
     }
 
-    function afterOrderExecution(bytes32 key, Order.Props memory order, EventUtils.EventLogData memory eventData) external returns(bool) {
-        return true;
+    function afterOrderExecution(bytes32 key, Order.Props memory order, EventUtils.EventLogData memory eventData) external {
     }
 
-    function afterOrderCancellation(bytes32 key, Order.Props memory order, EventUtils.EventLogData memory eventData) external returns(bool){
-        return true;
+    function afterOrderCancellation(bytes32 key, Order.Props memory order, EventUtils.EventLogData memory eventData) external {
     }
 
-    function afterOrderFrozen(bytes32 key, Order.Props memory order, EventUtils.EventLogData memory eventData) external returns(bool) {
-        return true;
+    function afterOrderFrozen(bytes32 key, Order.Props memory order, EventUtils.EventLogData memory eventData) external {
     }
 }
-
 
 
