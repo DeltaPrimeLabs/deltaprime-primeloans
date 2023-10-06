559ec1105d7592b3fedbde2ea6033e62a076acd7
mbare <mbare@deltaprime.io>
2023-10-06 17:37:09 +0200

    UI: disable direct Uni V3 ParaSwap swaps

--- src/components/AssetsTableRow.vue
+++ src/components/AssetsTableRow.vue
@@ -162,7 +162,7 @@ import ClaimGLPRewardsModal from './ClaimGLPRewardsModal';
 import {BigNumber} from 'ethers';
 import DeltaIcon from './DeltaIcon.vue';
 import IconButton from './IconButton.vue';
-import {constructSimpleSDK, SwapSide} from '@paraswap/sdk';
+import {constructSimpleSDK, ContractMethod, SwapSide} from '@paraswap/sdk';
 import axios from 'axios';
 import TradingViewChart from "./TradingViewChart.vue";
 import Toggle from "./Toggle.vue";
@@ -435,6 +435,8 @@ export default {
           amount: amountIn,
           userAddress: this.smartLoanContract.address,
           side: SwapSide.SELL,
+          includeContractMethods: [ContractMethod.simpleSwap],
+          excludeContractMethods: [ContractMethod.directUniV3Swap],
         });
         console.log('priceRoute');
         console.log(swapRate);
