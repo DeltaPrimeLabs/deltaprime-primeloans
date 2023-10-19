23c9991633b2c4050f63ef178ca0e694a04c02cb
Nidhock <nidhock@op.pl>
2023-10-19 16:41:26 +0200

    UI: PartnerInfoModal added

--- src/App.vue
+++ src/App.vue
@@ -92,6 +92,7 @@ const ethereum = window.ethereum;
 import Vue from 'vue';
 import Button from './components/Button';
 import ProgressBar from './components/ProgressBar';
+import PartnerInfoModal from './components/PartnerInfoModal';
 import ThemeToggle from "./components/ThemeToggle.vue";
 import {getCountdownString} from "./utils/calculate";
 
@@ -178,6 +179,9 @@ export default {
   },
 
   mounted() {
+    setTimeout(() => {
+      this.openModal(PartnerInfoModal)
+    }, 2000)
     window.testProperty = 'test value'
     document.addEventListener('keyup', (event) => {
       if (event.key === 'Escape') {
