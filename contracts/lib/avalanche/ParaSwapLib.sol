89ac19a7e40dc3a6af0977b9cb261c3380b5f3a3
0xlock <0xl0ck@proton.me>
2023-10-19 07:09:01 -0500

    implement megaSwap

--- contracts/lib/avalanche/ParaSwapLib.sol
+++ contracts/lib/avalanche/ParaSwapLib.sol
@@ -56,6 +56,25 @@ library ParaSwapLib {
         bytes16 uuid;
     }
 
+    struct MegaSwapPath {
+        uint256 fromAmountPercent;
+        Path[] path;
+    }
+
+    struct MegaSwapSellData {
+        address fromToken;
+        uint256 fromAmount;
+        uint256 toAmount;
+        uint256 expectedAmount;
+        address payable beneficiary;
+        MegaSwapPath[] path;
+        address payable partner;
+        uint256 feePercent;
+        bytes permit;
+        uint256 deadline;
+        bytes16 uuid;
+    }
+
     struct DirectUniV3 {
         address fromToken;
         address toToken;
@@ -76,15 +95,16 @@ library ParaSwapLib {
     bytes4 private constant SIMPLESWAP_SELECTOR = 0x54e3f31b;
     bytes4 private constant MULTISWAP_SELECTOR = 0xa94e78ef;
     bytes4 private constant DIRECT_UNIV3_SELECTOR = 0xa6886da9;
+    bytes4 private constant MEGASWAP_SELECTOR = 0x46c67b6d;
 
     function extractTokensAndAmount(bytes4 selector, bytes memory data) internal pure returns (address fromToken, address toToken, uint256 fromAmount) {
         if (selector == SIMPLESWAP_SELECTOR) {
             return getSimpleSwapData(data);
-        }
-        else if (selector == MULTISWAP_SELECTOR) {
+        } else if (selector == MULTISWAP_SELECTOR) {
             return getMultiSwapData(data);
-        }
-        else if (selector == DIRECT_UNIV3_SELECTOR) {
+        } else if (selector == MEGASWAP_SELECTOR) {
+            return getMegaSwapData(data);
+        } else if (selector == DIRECT_UNIV3_SELECTOR) {
             return getDirectUniV3SwapData(data);
         } else {
             revert ("Unknown Selector");
@@ -110,6 +130,14 @@ library ParaSwapLib {
             let toTokenOffsetFromPathArrayStart := add(mul(sub(pathArrayLength, 0x1), 0x54), 0x20) // (Path[].length - 1) * 84 bytes + 32 bytes offset (1st element of array is 32 bytes representing array length)
             toToken := shr(mul(0x0c, 0x08), mload(add(mload(add(data, 0xa8)), toTokenOffsetFromPathArrayStart)))
         }
+    }
+
+    function getMegaSwapData(bytes memory data) internal pure returns (address fromToken, address toToken, uint256 fromAmount) {
+        MegaSwapSellData memory swapData = abi.decode(data, (MegaSwapSellData));
+        MegaSwapPath memory lastMegaSwapPath = swapData.path[swapData.path.length - 1];
+        Path memory lastPath = lastMegaSwapPath.path[lastMegaSwapPath.path.length - 1];
+        toToken = lastPath.to;
+        (fromToken, fromAmount) = (swapData.fromToken, swapData.fromAmount);
     }
 
     function getDirectUniV3SwapData(bytes memory data) internal pure returns (address fromToken, address toToken, uint256 fromAmount) {
