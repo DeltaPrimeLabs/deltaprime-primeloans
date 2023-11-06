04a265605a47e6eaa304809655244cbbe812ec80
kavsky02 <kamilovsky@deltaprime.io>
2023-11-06 12:36:52 +0100

    Deploy tjv2 rewards claiming on Arbitrum

--- contracts/facets/arbitrum/TraderJoeV2ArbitrumFacet.sol
+++ contracts/facets/arbitrum/TraderJoeV2ArbitrumFacet.sol
@@ -9,11 +9,12 @@ contract TraderJoeV2ArbitrumFacet is TraderJoeV2Facet {
     }
 
     function getWhitelistedTraderJoeV2Pairs() internal pure override returns (ILBPair[] memory pools) {
-        pools = new ILBPair[](5);
+        pools = new ILBPair[](6);
         pools[0] = ILBPair(0x500173F418137090dad96421811147b63b448A0f);
         pools[1] = ILBPair(0xd387c40a72703B38A5181573724bcaF2Ce6038a5);
         pools[2] = ILBPair(0x94d53BE52706a155d27440C4a2434BEa772a6f7C);
         pools[3] = ILBPair(0x0Be4aC7dA6cd4bAD60d96FbC6d091e1098aFA358);
         pools[4] = ILBPair(0xcfA09B20c85933B197e8901226ad0D6dACa7f114);
+        pools[5] = ILBPair(0x60563686ca7b668e4a2d7D31448e5F10456ecaF8);
     }
 }
