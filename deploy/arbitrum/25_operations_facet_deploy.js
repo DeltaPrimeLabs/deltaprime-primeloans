2d5eaef4aedf7b123135212ff9f94335fe29eb9a
kavsky02 <kamilovsky@deltaprime.io>
2023-11-13 15:33:42 +0300

    Add AssetsOperationsAvalancheFacet.sol deployment files

--- deploy/arbitrum/25_operations_facet_deploy.js
+++ deploy/arbitrum/25_operations_facet_deploy.js
@@ -1,19 +1,11 @@
 import { embedCommitHash } from "../../tools/scripts/embed-commit-hash";
 
-const { ethers } = require("hardhat");
 import verifyContract from "../../tools/scripts/verify-contract";
 import hre from "hardhat";
-import {ether} from "@openzeppelin/test-helpers";
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer, admin } = await getNamedAccounts();
-
-    const FacetCutAction = {
-        Add: 0,
-        Replace: 1,
-        Remove: 2
-    }
 
     embedCommitHash("AssetsOperationsArbitrumFacet", "./contracts/facets/arbitrum");
 
@@ -35,40 +27,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             constructorArguments: []
         });
     console.log(`Verified AssetsOperationsArbitrumFacet`);
-    //
-    // const diamondContract = await ethers.getContract("SmartLoanDiamondBeacon");
-    // console.log(`Diamond address: ${diamondContract.address}`);
-    // const diamondCut = await ethers.getContractAt("IDiamondCut", diamondContract.address, deployer);
-    //
-    // let methodsSelectors = [
-    //     "0xe46bbc9e",
-    //     "0x040cf020",
-    //     "0x7c93ec30",
-    //     "0x00f989ad",
-    //     "0xd66e2979",
-    //     "0x9d9a355e",
-    //     "0x1281f5fe"
-    // ];
-    //
-    // const facetCut = [
-    //     [
-    //         AssetsOperationsArbitrumFacet.address,
-    //         FacetCutAction.Replace,
-    //         methodsSelectors
-    //     ]
-    // ]
-    //
-    // console.log(`Performing diamondCut with: ${facetCut}`)
-    // await diamondCut.pause();
-    // console.log('Paused')
-    // await diamondCut.diamondCut(
-    //     facetCut,
-    //     ethers.constants.AddressZero,
-    //     []
-    // )
-    // await diamondCut.unpause();
-    // console.log('Unpaused')
-    // console.log(`DiamondCut finished`)
 };
 
 module.exports.tags = ["arbitrum-operations-facet"];
