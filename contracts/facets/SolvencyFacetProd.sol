f9171d928d202be8df847b45d098bda14373cb32
kavsky02 <kamilovsky@deltaprime.io>
2023-11-18 14:37:23 +0300

    Bring back gas optimisation for tjv2 value calculations

--- contracts/facets/SolvencyFacetProd.sol
+++ contracts/facets/SolvencyFacetProd.sol
@@ -39,6 +39,13 @@ abstract contract SolvencyFacetProd is RedstoneConsumerNumericBase, DiamondHelpe
         AssetPrice[] assetsToRepayPrices;
     }
 
+    struct PriceInfo {
+        address tokenX;
+        address tokenY;
+        uint256 priceX;
+        uint256 priceY;
+    }
+
     /**
       * Checks if the loan is solvent.
       * It means that the Health Ratio is greater than 1e18.
@@ -474,7 +481,7 @@ abstract contract SolvencyFacetProd is RedstoneConsumerNumericBase, DiamondHelpe
 
         ITraderJoeV2Facet.TraderJoeV2Bin[] memory ownedTraderJoeV2Bins = DiamondStorageLib.getTjV2OwnedBinsView();
 
-        uint256[] memory prices = new uint256[](2);
+        PriceInfo memory priceInfo;
 
         if (ownedTraderJoeV2Bins.length > 0) {
             for (uint256 i; i < ownedTraderJoeV2Bins.length; i++) {
@@ -484,12 +491,19 @@ abstract contract SolvencyFacetProd is RedstoneConsumerNumericBase, DiamondHelpe
                 uint256 liquidity;
 
                 {
-                    bytes32[] memory symbols = new bytes32[](2);
+                    address tokenXAddress = address(binInfo.pair.getTokenX());
+                    address tokenYAddress = address(binInfo.pair.getTokenY());
 
-                    symbols[0] = DeploymentConstants.getTokenManager().tokenAddressToSymbol(address(binInfo.pair.getTokenX()));
-                    symbols[1] = DeploymentConstants.getTokenManager().tokenAddressToSymbol(address(binInfo.pair.getTokenY()));
+                    if (priceInfo.tokenX != tokenXAddress || priceInfo.tokenY != tokenYAddress) {
+                        bytes32[] memory symbols = new bytes32[](2);
 
-                    prices = getOracleNumericValuesFromTxMsg(symbols);
+
+                        symbols[0] = DeploymentConstants.getTokenManager().tokenAddressToSymbol(tokenXAddress);
+                        symbols[1] = DeploymentConstants.getTokenManager().tokenAddressToSymbol(tokenYAddress);
+
+                        uint256[] memory prices = getOracleNumericValuesFromTxMsg(symbols);
+                        priceInfo = PriceInfo(tokenXAddress, tokenYAddress, prices[0], prices[1]);
+                    }
                 }
 
                 {
@@ -497,9 +511,7 @@ abstract contract SolvencyFacetProd is RedstoneConsumerNumericBase, DiamondHelpe
 
                     price = PriceHelper.convert128x128PriceToDecimal(binInfo.pair.getPriceFromId(binInfo.id)); // how is it denominated (what precision)?
 
-                    liquidity = price * binReserveX
-                    / 10 ** IERC20Metadata(address(binInfo.pair.getTokenX())).decimals()
-                    + binReserveY;
+                    liquidity = price * binReserveX / 10 ** 18 + binReserveY;
                 }
 
 
@@ -509,8 +521,11 @@ abstract contract SolvencyFacetProd is RedstoneConsumerNumericBase, DiamondHelpe
 
                     total = total +
                     Math.min(
-                        debtCoverageX * liquidity * prices[0] / (price * 10 ** 8),
-                        debtCoverageY * liquidity / 10 ** IERC20Metadata(address(binInfo.pair.getTokenY())).decimals() * prices[1] / 10 ** 8
+                        price > 10**24 ?
+                            debtCoverageX * liquidity / (price / 10 ** 18) / 10 ** IERC20Metadata(address(binInfo.pair.getTokenX())).decimals() * priceInfo.priceX / 10 ** 8
+                            :
+                            debtCoverageX * liquidity / price * 10**18 / 10 ** IERC20Metadata(address(binInfo.pair.getTokenX())).decimals() * priceInfo.priceX / 10 ** 8,
+                        debtCoverageY * liquidity / 10**(IERC20Metadata(address(binInfo.pair.getTokenY())).decimals()) * priceInfo.priceY / 10 ** 8
                     )
                     .mulDivRoundDown(binInfo.pair.balanceOf(address(this), binInfo.id), 1e18)
                     .mulDivRoundDown(1e18, binInfo.pair.totalSupply(binInfo.id));
