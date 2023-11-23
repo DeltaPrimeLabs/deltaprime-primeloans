90db270b5cc600c1ec7efb8cb8d875febf2d6a9e
mbare <mbare@deltaprime.io>
2023-11-23 18:15:15 +0700

    Update GMX V2 interfaces

--- contracts/interfaces/facets/arbitrum/IGmxV2ArbitrumFacet.sol
+++ contracts/interfaces/facets/arbitrum/IGmxV2ArbitrumFacet.sol
@@ -12,6 +12,10 @@ interface IGmxV2ArbitrumFacet {
 
     function withdrawArbUsdcGmxV2(uint256 gmAmount, uint256 minLongTokenAmount, uint256 minShortTokenAmount, uint256 executionFee) external payable;
 
+    function depositBtcUsdcGmxV2(bool isLongToken, uint256 tokenAmount, uint256 minGmAmount, uint256 executionFee) external payable;
+
+    function withdrawBtcUsdcGmxV2(uint256 gmAmount, uint256 minLongTokenAmount, uint256 minShortTokenAmount, uint256 executionFee) external payable;
+
     function afterDepositExecution(bytes32 key, Deposit.Props memory deposit, EventUtils.EventLogData memory eventData) external;
 
     function afterDepositCancellation(bytes32 key, Deposit.Props memory deposit, EventUtils.EventLogData memory eventData) external;
--- contracts/interfaces/facets/avalanche/IGmxV2AvalancheFacet.sol
+++ contracts/interfaces/facets/avalanche/IGmxV2AvalancheFacet.sol
@@ -7,4 +7,5 @@ interface IGmxV2AvalancheFacet {
     function depositAvaxUsdcGmxV2(bool isLongToken, uint256 tokenAmount, uint256 minGmAmount, uint256 executionFee) external payable;
 
     function withdrawAvaxUsdcGmxV2(uint256 gmAmount, uint256 minLongTokenAmount, uint256 minShortTokenAmount, uint256 executionFee) external payable;
+
 }
