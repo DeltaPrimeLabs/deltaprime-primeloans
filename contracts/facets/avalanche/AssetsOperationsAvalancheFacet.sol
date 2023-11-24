c10d3f269f0b45c25859e418ba71fb5e8256f445
kavsky02 <kamilovsky@deltaprime.io>
2023-11-24 14:04:31 +0100

    Deployment files

--- contracts/facets/avalanche/AssetsOperationsAvalancheFacet.sol
+++ contracts/facets/avalanche/AssetsOperationsAvalancheFacet.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: 6685d23f65961b7e5a0dbbbb475e15987fb1954f;
+// Last deployed from commit: cd227651daba2756b0ed59623fd404c32885e63d;
 pragma solidity 0.8.17;
 
 import "../AssetsOperationsFacet.sol";
@@ -10,9 +10,9 @@ contract AssetsOperationsAvalancheFacet is AssetsOperationsFacet {
         return 0xC4729E56b831d74bBc18797e0e17A295fA77488c;
     }
 
-    function processYieldYakPTPRefundBeforeLiquidation() external onlyWhitelistedLiquidators {
-        DiamondStorageLib.addOwnedAsset("sAVAX", 0x2b2C81e08f1Af8835a78Bb2A90AE924ACE0eA4bE);
-        DiamondStorageLib.removeOwnedAsset("YY_PTP_sAVAX");
+    function processYieldYakTJsAVAXLPRefundBeforeLiquidation() external onlyWhitelistedLiquidators {
+        DiamondStorageLib.addOwnedAsset("TJ_AVAX_sAVAX_LP", 0x4b946c91C2B1a7d7C40FB3C130CdfBaf8389094d);
+        DiamondStorageLib.removeOwnedAsset("YY_TJ_AVAX_sAVAX_LP");
     }
 
 
