7557d76f8ec525c370a535b028cce7035b61de75
kavsky02 <kamilovsky@deltaprime.io>
2023-11-30 13:27:24 +0100

    Add deployment files

--- contracts/facets/arbitrum/AssetsOperationsArbitrumFacet.sol
+++ contracts/facets/arbitrum/AssetsOperationsArbitrumFacet.sol
@@ -1,16 +1,33 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: f0764fb93f51c64f2def5256317eacea84e45913;
+// Last deployed from commit: 8479ca6cbfea247ea80210166dd068af1228914f;
 pragma solidity 0.8.17;
 
 import "../AssetsOperationsFacet.sol";
 
 contract AssetsOperationsArbitrumFacet is AssetsOperationsFacet {
+    using TransferHelper for address payable;
+    using TransferHelper for address;
+
     function YY_ROUTER() internal override pure returns (address) {
         return 0xb32C79a25291265eF240Eb32E9faBbc6DcEE3cE3;
     }
 
-    function removeDpxAssets() external {
-        DiamondStorageLib.removeOwnedAsset("DPX");
-        DiamondStorageLib.removeOwnedAsset("SUSHI_DPX_ETH_LP");
+    /**
+    * Funds the loan with a specified amount of a GLP
+    * @dev Requires approval for stakedGLP token on frontend side
+    * @param _amount to be funded
+    **/
+    function fundGLP(uint256 _amount) public override {
+        IERC20Metadata stakedGlpToken = IERC20Metadata(0x5402B5F40310bDED796c7D0F3FF6683f5C0cFfdf);
+        _amount = Math.min(_amount, stakedGlpToken.balanceOf(msg.sender));
+        address(stakedGlpToken).safeTransferFrom(msg.sender, address(this), _amount);
+        if (stakedGlpToken.balanceOf(address(this)) > 0) {
+            DiamondStorageLib.addOwnedAsset("GLP", address(stakedGlpToken));
+        }
+
+        ITokenManager tokenManager = DeploymentConstants.getTokenManager();
+        tokenManager.increaseProtocolExposure("GLP", _amount);
+
+        emit Funded(msg.sender, "GLP", _amount, block.timestamp);
     }
 }
