f2cb43bc0bb995e1465345de1fb1c575fc3c1c0f
mbare <mbare@deltaprime.io>
2023-12-01 21:25:46 +0700

    Script for checking Boost APY for GM grant

--- tools/scripts/check-loan-data.js
+++ tools/scripts/check-loan-data.js
@@ -1,61 +1,110 @@
+import {WrapperBuilder} from "@redstone-finance/evm-connector";
+import config from "../../src/config";
+import CACHE_LAYER_URLS from "../../common/redstone-cache-layer-urls.json";
+import addresses from "../../common/addresses/arbitrum/token_addresses.json";
+const fetch = require("node-fetch");
+
 const loanAddress = "0x4a4B8C28922FfC8F1E7Ce0969B49962a926B1184";
-const jsonRPC = "https://api.avax.network/ext/bc/C/rpc";
+const jsonRPC = "https://arbitrum-mainnet.core.chainstack.com/9a30fb13b2159a76c8e143c52d5579bf";
 
 const ARTIFACT = require(`../../artifacts/contracts/interfaces/SmartLoanGigaChadInterface.sol/SmartLoanGigaChadInterface.json`);
+const FACTORY_ARTIFACT = require(`../../artifacts/contracts/SmartLoansFactory.sol/SmartLoansFactory.json`);
 const ethers = require("ethers");
 const fs = require("fs");
-const {wrapContract} = require("../../src/utils/blockchain");
-const {fromWei, fromBytes32} = require("../../test/_helpers");
 
 const key = fs.readFileSync("./.secret").toString().trim();
 let mnemonicWallet = new ethers.Wallet(key);
 let provider = new ethers.providers.JsonRpcProvider(jsonRPC);
 let wallet = mnemonicWallet.connect(provider);
 
-let loan = new ethers.Contract(loanAddress, ARTIFACT.abi, wallet);
+const fromWei = val => parseFloat(ethers.utils.formatEther(val));
+const fromBytes32 = ethers.utils.parseBytes32String;
+
 let wrappedLoan;
+
+let factory = new ethers.Contract('0xFf5e3dDaefF411a1dC6CcE00014e4Bca39265c20', FACTORY_ARTIFACT.abi, wallet);
 
 run().then(() => console.log('Finished!'))
 
+let totalLeveragedGm = 0;
+let totalGmsBalance = 0;
+
 async function run() {
-    await prepare();
-    await getData();
+    let loans = await factory.getAllLoans();
+
+    const redstonePriceDataRequest = await fetch('https://oracle-gateway-2.a.redstone.finance/data-packages/latest/redstone-arbitrum-prod');
+    const redstonePriceData = await redstonePriceDataRequest.json();
+
+    let i = 0;
+    for (let loan of loans) {
+        let [leveragedGm, gmsBalance] = await getData(loan, redstonePriceData);
+        totalLeveragedGm += leveragedGm;
+        totalGmsBalance += gmsBalance;
+        console.log(`totalLeveragedGm for ${i + 1} loans: `, totalLeveragedGm);
+        i++;
+    }
+
+    const boostApy = 10000 / 7 * 365 / totalLeveragedGm;
+
+    console.log('totalLeveragedGm: ', totalLeveragedGm);
+    console.log('totalGmsBalance: ', totalGmsBalance);
+    console.log('boostApy: ', boostApy);
 }
 
-async function prepare() {
+
+
+async function getData(loanAddress, redstonePriceData) {
+    let loan = new ethers.Contract(loanAddress, ARTIFACT.abi, wallet);
     wrappedLoan = await wrapContract(loan);
-}
-
-async function getData() {
-    console.log('getData')
-    // await vectorFinanceRewards('0x91F78865b239432A1F1Cc1fFeC0Ac6203079E6D7', loanAddress, 18, wallet.provider.getSigner(wallet.address))
-
-    console.log('Balances: ');
 
     let balances = await loan.getAllAssetsBalances();
-    balances.forEach(
+    balances = balances.map(
         balance => {
-            console.log('Symbol: ', fromBytes32(balance[0]))
-            console.log('Balance in wei: ', balance[1].toString())
+            return [fromBytes32(balance[0]), fromWei(balance[1])]
         });
-
-    console.log('Staked positions: ')
-
-    let positions = await loan.getStakedPositions()
-    positions.forEach(
-        (position, i) => {
-            console.log('Position ', i)
-            console.log('Asset address: ', position[0])
-            console.log('Symbol: ', fromBytes32(position[1]))
-            console.log('Identifier: ', fromBytes32(position[2]))
-        });
-
-    console.log('Loan status: ',)
 
     let status = (await wrappedLoan.getFullLoanStatus()).map(el => fromWei(el));
-    console.log('Total value: ', status[0])
-    console.log('Debt: ', status[1])
-    console.log('TWV: ', status[2])
-    console.log('Health: ', status[3])
-    console.log('Solvent: ', status[4] === 1e-18)
+
+    let collateral = status[0] - status[1];
+
+    let gmEthWorth = balances.find(bal => bal[0] == 'GM_ETH_WETH_USDC')[1] * redstonePriceData['GM_ETH_WETH_USDC'][0].dataPoints[0].value;
+    let gmBtcWorth = balances.find(bal => bal[0] == 'GM_BTC_WBTC_USDC')[1] * redstonePriceData['GM_BTC_WBTC_USDC'][0].dataPoints[0].value;
+    let gmArbWorth = balances.find(bal => bal[0] == 'GM_ARB_ARB_USDC')[1] * redstonePriceData['GM_ARB_ARB_USDC'][0].dataPoints[0].value;
+    let gmUniWorth = balances.find(bal => bal[0] == 'GM_UNI_UNI_USDC')[1] * redstonePriceData['GM_UNI_UNI_USDC'][0].dataPoints[0].value;
+    let gmLinkWorth = balances.find(bal => bal[0] == 'GM_LINK_LINK_USDC')[1] * redstonePriceData['GM_LINK_LINK_USDC'][0].dataPoints[0].value;
+
+    let gmBalance =
+        balances.find(bal => bal[0] == 'GM_ETH_WETH_USDC')[1] +
+        balances.find(bal => bal[0] == 'GM_BTC_WBTC_USDC')[1] +
+        balances.find(bal => bal[0] == 'GM_ARB_ARB_USDC')[1] +
+        balances.find(bal => bal[0] == 'GM_UNI_UNI_USDC')[1] +
+        balances.find(bal => bal[0] == 'GM_LINK_LINK_USDC')[1];
+
+
+    let gmWorth = gmEthWorth + gmBtcWorth + gmArbWorth + gmUniWorth + gmLinkWorth;
+
+    let leveragedGm = gmWorth - collateral > 0 ? gmWorth - collateral : 0;
+
+    console.log('--------------------')
+    console.log('loanAddress: ', loanAddress)
+    console.log('collateral: ', collateral)
+    console.log('gmWorth: ', gmWorth)
+    console.log('leveragedGm: ', leveragedGm)
+
+    return [leveragedGm, gmBalance];
 }
+
+const wrapContract = async function wrapContract(contract, assets) {
+    //for more symbols in data feed it's more optimal to not specify asset list
+    const providedAssets = (assets && assets.length <= 5) ? assets : undefined;
+
+    return WrapperBuilder.wrap(contract).usingDataService(
+        {
+            dataServiceId: 'redstone-arbitrum-prod',
+            uniqueSignersCount: 3,
+            dataFeeds: providedAssets,
+            disablePayloadsDryRun: true
+        },
+        CACHE_LAYER_URLS.urls
+    );
+};
