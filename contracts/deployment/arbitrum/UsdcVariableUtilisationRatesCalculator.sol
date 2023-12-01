6679faa46a6f971a4b28803fbb45524e2d5f30d5
kavsky02 <kamilovsky@deltaprime.io>
2023-12-01 19:53:46 +0100

    Add deployment files Arbi RatesCalculators

--- contracts/deployment/arbitrum/UsdcVariableUtilisationRatesCalculator.sol
+++ contracts/deployment/arbitrum/UsdcVariableUtilisationRatesCalculator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: 9331e1dc457b940b0afd98019e171a23dff020bd;
+// Last deployed from commit: fe39e3c3a44a64b31f730037a97816cae5a8f3c7;
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -14,25 +14,25 @@ import "../../interfaces/IRatesCalculator.sol";
  * which second piece is considered) and MAX_RATE (value at pool utilisation of 1).
  **/
 contract UsdcVariableUtilisationRatesCalculator is IRatesCalculator, Ownable {
-    uint256 public constant SLOPE_1 = 0.05e18;
+    uint256 public constant SLOPE_1 = 0.167e18;
     uint256 public constant OFFSET_1 = 0;
 
     uint256 public constant BREAKPOINT_1 = 0.6e18;
 
-    uint256 public constant SLOPE_2 = 0.2e18;
+    uint256 public constant SLOPE_2 = 0.25e18;
     //negative, hence minus in calculations
     uint256 public constant OFFSET_2 = 0.09e18;
 
     uint256 public constant BREAKPOINT_2 = 0.8e18;
 
-    uint256 public constant SLOPE_3 = 0.5e18;
+    uint256 public constant SLOPE_3 = 1e18;
     //negative, hence minus in calculations
     uint256 public constant OFFSET_3 = 0.33e18;
 
     // BREAKPOINT must be lower than 1e18
     uint256 public constant BREAKPOINT_3 = 0.9e18;
 
-    uint256 public constant SLOPE_4 = 7.800e18;
+    uint256 public constant SLOPE_4 = 6.5e18;
     //negative, hence minus in calculations
     uint256 public constant OFFSET_4 = 6.9e18;
 
