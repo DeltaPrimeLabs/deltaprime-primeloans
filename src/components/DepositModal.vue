ae4808987f478563977b9613b70e7aab0c317140
mbare <mbare@deltaprime.io>
2023-12-09 20:07:45 +0700

    UI: update sPRIME emissions

--- src/components/DepositModal.vue
+++ src/components/DepositModal.vue
@@ -127,8 +127,8 @@ export default {
 
     miningApy() {
       if (!this.pool || this.pool.tvl === 0) return 0;
-      return (config.chainId === 42161) ?  1000 * 365 / 4 / (this.pool.tvl * this.pool.assetPrice)
-          : Math.max((1 - this.pool.tvl * this.pool.assetPrice / 4000000) * 0.1, 0);
+      return (config.chainId === 42161) ?  0.75 * 1000 * 365 / 4 / (this.pool.tvl * this.pool.assetPrice)
+          : 0.75 * Math.max((1 - this.pool.tvl * this.pool.assetPrice / 4000000) * 0.1, 0);
       },
 
     getModalHeight() {
