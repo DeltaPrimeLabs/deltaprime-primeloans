c9a0d9f57b345131acb8e926775476597d6dc652
kavsky02 <kamilovsky@deltaprime.io>
2024-01-08 14:53:03 +0100

    Add proper override methods to AssetsOperationsArbitrumFacet.sol with Arbi GLP address

--- contracts/facets/arbitrum/AssetsOperationsArbitrumFacet.sol
+++ contracts/facets/arbitrum/AssetsOperationsArbitrumFacet.sol
@@ -30,4 +30,24 @@ contract AssetsOperationsArbitrumFacet is AssetsOperationsFacet {
 
         emit Funded(msg.sender, "GLP", _amount, block.timestamp);
     }
+
+    /**
+        * Withdraws specified amount of a GLP
+        * @param _amount to be withdrawn
+    **/
+    function withdrawGLP(uint256 _amount) public virtual onlyOwner nonReentrant canRepayDebtFully remainsSolvent{
+        IERC20Metadata token = getERC20TokenInstance("GLP", true);
+        IERC20Metadata stakedGlpToken = IERC20Metadata(0x5402B5F40310bDED796c7D0F3FF6683f5C0cFfdf);
+        _amount = Math.min(token.balanceOf(address(this)), _amount);
+
+        address(stakedGlpToken).safeTransfer(msg.sender, _amount);
+        if (token.balanceOf(address(this)) == 0) {
+            DiamondStorageLib.removeOwnedAsset("GLP");
+        }
+
+        ITokenManager tokenManager = DeploymentConstants.getTokenManager();
+        tokenManager.decreaseProtocolExposure("GLP", _amount);
+
+        emit Withdrawn(msg.sender, "GLP", _amount, block.timestamp);
+    }
 }
