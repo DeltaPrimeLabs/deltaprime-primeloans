68528a88d49abd4b85240a1a6eeb420f50ec8741
kavsky02 <kamilovsky@deltaprime.io>
2024-02-08 15:23:31 +0100

    Allow unlimited contract size on a local fork

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,7 +26,7 @@ export default {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 50
+            runs: 10
           }
         }
       },
@@ -38,11 +38,16 @@ export default {
       forking: {
         url: "https://arb1.arbitrum.io/rpc",
       },
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
     },
     localhost: {
       timeout: 1800000,
       url: 'http://127.0.0.1:8545/',
       chainId: 31337,
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
       allowUnlimitedContractSize: true,
       // accounts: [getKey('avalanche', 'deployer'), getKey('avalanche', 'admin')]
     },
