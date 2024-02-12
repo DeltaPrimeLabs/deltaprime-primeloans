2c04f8bbc145fcb8a91c9e51438a65729a4a81fc
kavsky02 <kamilovsky@deltaprime.io>
2024-02-12 16:48:05 +0100

    TokenManager add deployment files avalanche

--- contracts/TokenManager.sol
+++ contracts/TokenManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: dcc1e8a29244595e90da7731e65e43122519b3a5;
+// Last deployed from commit: 163cb6d95659cf59c2c6c38001dc26adb791e5bc;
 pragma solidity 0.8.17;
 
 import "./lib/Bytes32EnumerableMap.sol";
