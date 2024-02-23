542e7320d8a1de92ed42e3e8e3d40619ee355b1a
LeCh1ffre <lechiffre@deltaprime.io>
2024-02-23 20:44:52 +0100

    UI: Avalanche chain down banner - remove

--- src/App.vue
+++ src/App.vue
@@ -171,7 +171,7 @@ export default {
         // this.showAvalancheDepositorBanner = true;
       }
       if (window.location.href.includes('prime-account')) {
-        this.showAvalanchePrimeAccountBanner = true;
+        // this.showAvalanchePrimeAccountBanner = true;
       }
     }
   },
