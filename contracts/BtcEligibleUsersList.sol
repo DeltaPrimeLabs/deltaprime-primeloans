151cf7c54e924244e38020e42043edc1083e952b
kavsky02 <kamilovsky@deltaprime.io>
2024-02-27 15:20:27 +0100

    Refactor approach

---
+++ contracts/BtcEligibleUsersList.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BtcEligibleUsersList is Ownable{
+    address[] public eligibleUsersList;
+
+    constructor(address[] memory _eligibleUsersList) Ownable(0xBd2413135f3aab57195945A046cCA4e4bacD5a5b){
+        eligibleUsersList = _eligibleUsersList;
+    }
+
+    // only owner function to add users to eligible list
+    function addEligibleUsers(address[] calldata _eligibleUsers) external onlyOwner {
+        for (uint256 i = 0; i < _eligibleUsers.length; i++) {
+            eligibleUsersList.push(_eligibleUsers[i]);
+        }
+    }
+
+    /// only owner function to remove users from eligible list
+    function removeEligibleUsers(address[] calldata _eligibleUsers) external onlyOwner {
+        for (uint256 i = 0; i < _eligibleUsers.length; i++) {
+            for (uint256 j = 0; j < eligibleUsersList.length; j++) {
+                if (eligibleUsersList[j] == _eligibleUsers[i]) {
+                    eligibleUsersList[j] = eligibleUsersList[eligibleUsersList.length - 1];
+                    eligibleUsersList.pop();
+                }
+            }
+        }
+    }
+
+    // only owner function for replacing eligible users list
+    function replaceEligibleUsersList(address[] calldata _eligibleUsersList) external onlyOwner {
+        eligibleUsersList = _eligibleUsersList;
+    }
+
+    // function for getting the number of eligible users
+    function getEligibleUsersCount() external view returns (uint256) {
+        return eligibleUsersList.length;
+    }
+
+    // function for getting range of eligible users
+    function getEligibleUsers(uint256 _start, uint256 _end) external view returns (address[] memory) {
+        require(_start < _end, "Invalid range");
+        require(_end <= eligibleUsersList.length, "Invalid range");
+        address[] memory users = new address[](_end - _start);
+        for (uint256 i = _start; i < _end; i++) {
+            users[i - _start] = eligibleUsersList[i];
+        }
+        return users;
+    }
+}
--- contracts/RandomTokenRewarder.sol
+++ contracts/RandomTokenRewarder.sol
@@ -5,8 +5,7 @@ import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/Confir
 import {VRFV2WrapperConsumerBase} from "@chainlink/contracts/src/v0.8/vrf/VRFV2WrapperConsumerBase.sol";
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./SmartLoansFactory.sol";
-import "./Pool.sol";
+import "./BtcEligibleUsersList.sol";
 
 
 contract RandomTokenRewarder is VRFV2WrapperConsumerBase, ConfirmedOwner {
@@ -15,17 +14,12 @@ contract RandomTokenRewarder is VRFV2WrapperConsumerBase, ConfirmedOwner {
     // event for transferring reward to winner
     event RewardTransferred(address indexed winner, address indexed rewardToken, uint256 amount, uint256 blockNumber);
 
-    // past requests Id.
-    uint256 public lastRandomNumber;
-    bool public lastRandomNumberUsed;
-    uint256 public indexIncrementer;
-
     // Depends on the number of requested values that you want sent to the
     // fulfillRandomWords() function. Test and adjust
     // this limit based on the network that you select, the size of the request,
     // and the processing of the callback request in the fulfillRandomWords()
     // function.
-    uint32 callbackGasLimit = 14000000;
+    uint32 callbackGasLimit = 1000000;
 
     // The default is 3, but you can set this higher.
     uint16 requestConfirmations = 3;
@@ -33,19 +27,8 @@ contract RandomTokenRewarder is VRFV2WrapperConsumerBase, ConfirmedOwner {
     // For this example, retrieve 2 random values in one request.
     // Cannot exceed VRFV2Wrapper.getConfig().maxNumWords.
     uint32 numWords = 1;
-
-    uint256 public minimumGmBtcAmount = 100 * 1e18;
-    uint256 public minimumGmBtcBorrowed = 187600;
-    address public rewardToken = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
-
-    // setters for above 3 variables
-    function setMinimumGmBtcAmount(uint256 _minimumGmBtcAmount) external onlyOwner {
-        minimumGmBtcAmount = _minimumGmBtcAmount;
-    }
-
-    function setMinimumGmBtcBorrowed(uint256 _minimumGmBtcBorrowed) external onlyOwner {
-        minimumGmBtcBorrowed = _minimumGmBtcBorrowed;
-    }
+    address public rewardToken = 0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e;
+    BtcEligibleUsersList public btcEligibleUsersList;
 
     function setRewardToken(address _rewardToken) external onlyOwner {
         rewardToken = _rewardToken;
@@ -60,17 +43,13 @@ contract RandomTokenRewarder is VRFV2WrapperConsumerBase, ConfirmedOwner {
     address public linkAddress = 0x5947BB275c521040051D82396192181b413227A3;
     // Address WRAPPER
     address public wrapperAddress = 0x721DFbc5Cfe53d32ab00A9bdFa605d3b8E1f3f42;
-    // Address SmartLoansFactoryTUP
-    address public smartLoansFactoryTUP = 0x3Ea9D480295A73fd2aF95b4D96c2afF88b21B03D;
-    // Address DPBTC pool TUP
-    address public btcPoolTUP = 0x475589b0Ed87591A893Df42EC6076d2499bB63d0;
-    // Address GM BTC Avalanche
-    address public gmBtcUsdcAvalanche = 0x913C1F46b48b3eD35E7dc3Cf754d4ae8499F31CF;
 
-    constructor()
+    constructor(address _btcEligibleUsersList)
     ConfirmedOwner(msg.sender)
     VRFV2WrapperConsumerBase(linkAddress, wrapperAddress)
-    {}
+    {
+        btcEligibleUsersList = BtcEligibleUsersList(_btcEligibleUsersList);
+    }
 
     function requestRewardDistributionWithRandomWords()
     external
@@ -86,69 +65,17 @@ contract RandomTokenRewarder is VRFV2WrapperConsumerBase, ConfirmedOwner {
         return requestId;
     }
 
-    function isAccountEligible(address primeAccount) internal view returns (bool){
-        return (
-            Pool(btcPoolTUP).getBorrowed(primeAccount) >= minimumGmBtcBorrowed &&
-            IERC20(gmBtcUsdcAvalanche).balanceOf(primeAccount) >= minimumGmBtcAmount
-        );
-    }
-
-    function continueDistributionProcess() external onlyOwner{
-        if(lastRandomNumberUsed){
-            revert("Random number was already used");
-        } else{
-            address primeAccountAddress = findNextEligiblePrimeAccountAddress();
-            uint256 rewardBalance = IERC20(rewardToken).balanceOf(address(this));
-            lastRandomNumberUsed = true;
-            IERC20(rewardToken).transfer(primeAccountAddress, rewardBalance);
-            emit RewardTransferred(primeAccountAddress, rewardToken, rewardBalance, block.number);
-        }
-    }
-
-    function findNextEligiblePrimeAccountAddress() internal returns (address){
-        SmartLoansFactory slf = SmartLoansFactory(smartLoansFactoryTUP);
-        uint256 numberOfPrimeAccounts = slf.getLoansLength();
-        uint256 primeAccountIndex = (lastRandomNumber % numberOfPrimeAccounts) + indexIncrementer;
-        if(primeAccountIndex >= numberOfPrimeAccounts){
-            primeAccountIndex = primeAccountIndex - numberOfPrimeAccounts;
-        }
-
-        bool found = false;
-        while(!found){
-            if(gasleft() < 100000){
-                indexIncrementer = primeAccountIndex;
-                return address(0);
-            }
-            if(isAccountEligible(slf.getLoans(primeAccountIndex, 1)[0])){
-                found = true;
-                break;
-            }
-            if(primeAccountIndex == numberOfPrimeAccounts){
-                primeAccountIndex = 0;
-            } else {
-                primeAccountIndex++;
-            }
-        }
-        return slf.getLoans(primeAccountIndex, 1)[0];
-    }
 
     function fulfillRandomWords(
         uint256 _requestId,
         uint256[] memory _randomWords
     ) internal override {
         uint256 randomWord = _randomWords[0];
-        SmartLoansFactory slf = SmartLoansFactory(smartLoansFactoryTUP);
 
-        lastRandomNumber = randomWord;
-        lastRandomNumberUsed = false;
-
-        address primeAccountAddress = findNextEligiblePrimeAccountAddress();
-        if(primeAccountAddress == address(0)){
-            return;
-        }
+        uint256 primeAccountCount = btcEligibleUsersList.getEligibleUsersCount();
+        address primeAccountAddress = btcEligibleUsersList.getEligibleUsers(randomWord % primeAccountCount, 1)[0];
 
         uint256 rewardBalance = IERC20(rewardToken).balanceOf(address(this));
-        lastRandomNumberUsed = true;
         IERC20(rewardToken).transfer(primeAccountAddress, rewardBalance);
         emit RewardTransferred(primeAccountAddress, rewardToken, rewardBalance, block.number);
     }
