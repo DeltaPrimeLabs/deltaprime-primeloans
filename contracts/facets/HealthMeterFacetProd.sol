6032f3c2c594311b1717c20680c609bfa8281681
kavsky02 <kamilovsky@deltaprime.io>
2024-02-29 17:50:03 +0100

    Deploy HealthMeterFacetProd.sol avalanche

--- contracts/facets/HealthMeterFacetProd.sol
+++ contracts/facets/HealthMeterFacetProd.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: d5641d2d8be5d188d6e4f4f272ae32174783f6a1;
+// Last deployed from commit: 08adb0d77de54931fd6392c0641b77785f30cf56;
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
