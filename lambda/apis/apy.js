c899512e0dbc1c0cfcd89a03b831d5b8c50be41c
keizir <ultimatesofteng.work@gmail.com>
2024-02-29 11:37:54 +0800

    Lambda: add API to retrieve APYs

--- lambda/apis/apy.js
+++ lambda/apis/apy.js
@@ -25,6 +25,27 @@ const levelTvlApi = (event, context, callback) => {
     });
 }
 
+const assetApyApi = (event, context, callback) => {
+  const params = {
+    TableName: process.env.APY_TABLE,
+  };
+
+  dynamoDb.scan(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Items),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch APYs.'));
+      return;
+    });
+};
+
 module.exports = {
-  levelTvlApi
+  levelTvlApi,
+  assetApyApi
 }
