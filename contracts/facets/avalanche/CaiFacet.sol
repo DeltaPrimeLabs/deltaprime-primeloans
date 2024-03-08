595447fdab3481dd5cdeebbd6d4e3858220c3fce
0xlock <0xl0ck@proton.me>
2024-03-08 07:01:16 -0600

    Deploy CaiFacet.sol
    CaiFacet.sol deployment files
    
    Remove recalculateAssetsExposure modifier from CAI
    
    update exposure calculation
    
    hardcode env variables
    
    implement CAI mint/burn

---
+++ contracts/facets/avalanche/CaiFacet.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: BUSL-1.1
+// Last deployed from commit: 6d86e26ff5a23ee2fa4ddb079d0569c41d45f7dd;
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "../../ReentrancyGuardKeccak.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import {DiamondStorageLib} from "../../lib/DiamondStorageLib.sol";
+import "../../OnlyOwnerOrInsolvent.sol";
+import "../../interfaces/IWrappedNativeToken.sol";
+
+//This path is updated during deployment
+import "../../lib/local/DeploymentConstants.sol";
+
+contract CaiFacet is ReentrancyGuardKeccak, OnlyOwnerOrInsolvent {
+    using TransferHelper for address;
+
+    address private constant INDEX_ROUTER =
+        0xD6dd95610fC3A3579a2C32fe06158d8bfB8F4eE9;
+
+    address private constant CAI = 0x48f88A3fE843ccb0b5003e70B4192c1d7448bEf0;
+
+    function mintCai(
+        bytes4 selector,
+        bytes memory data,
+        address fromToken,
+        uint256 fromAmount,
+        uint256 minOut
+    ) external nonReentrant onlyOwner remainsSolvent {
+        ITokenManager tokenManager = DeploymentConstants.getTokenManager();
+
+        require(minOut > 0, "minOut needs to be > 0");
+        require(
+            fromAmount > 0,
+            "Amount of tokens to sell has to be greater than 0"
+        );
+
+        bytes32 fromTokenSymbol = tokenManager.tokenAddressToSymbol(fromToken);
+
+        address(fromToken).safeApprove(INDEX_ROUTER, 0);
+        address(fromToken).safeApprove(INDEX_ROUTER, fromAmount);
+
+        uint256 beforeCaiBalance = IERC20(CAI).balanceOf(address(this));
+        uint256 beforeFromTokenBalance = IERC20(fromToken).balanceOf(address(this));
+
+        (bool success, ) = INDEX_ROUTER.call((abi.encodePacked(selector, data)));
+        require(success, "Mint failed");
+
+        uint256 afterCaiBalance = IERC20(CAI).balanceOf(address(this));
+        uint256 afterFromTokenBalance = IERC20(fromToken).balanceOf(address(this));
+
+        address(CAI).safeApprove(INDEX_ROUTER, 0);
+        address(CAI).safeApprove(INDEX_ROUTER, type(uint256).max);
+
+        uint256 caiBoughtAmount = afterCaiBalance - beforeCaiBalance;
+        require(caiBoughtAmount >= minOut, "Too little received");
+
+        _increaseExposure(tokenManager, CAI, caiBoughtAmount);
+        _decreaseExposure(tokenManager, fromToken, beforeFromTokenBalance - afterFromTokenBalance);
+
+        emit CaiMinted(
+            msg.sender,
+            fromTokenSymbol,
+            fromAmount,
+            caiBoughtAmount,
+            block.timestamp
+        );
+    }
+
+    function burnCai(
+        bytes4 selector,
+        bytes memory data,
+        uint256 shares,
+        address toToken,
+        uint256 minOut
+    ) external nonReentrant onlyOwnerOrInsolvent remainsSolvent {
+        ITokenManager tokenManager = DeploymentConstants.getTokenManager();
+
+        require(minOut > 0, "minOut needs to be > 0");
+        require(shares > 0, "Amount of tokens to sell has to be greater than 0");
+
+        bytes32 toTokenSymbol = tokenManager.tokenAddressToSymbol(toToken);
+
+        address(CAI).safeApprove(INDEX_ROUTER, 0);
+        address(CAI).safeApprove(INDEX_ROUTER, shares);
+
+        uint256 beforeCaiBalance = IERC20(CAI).balanceOf(address(this));
+        uint256 beforeTokenBalance = IERC20(toToken).balanceOf(address(this));
+
+        (bool success, ) = INDEX_ROUTER.call((abi.encodePacked(selector, data)));
+        require(success, "Burn failed");
+
+        uint256 afterCaiBalance = IERC20(CAI).balanceOf(address(this));
+        uint256 afterTokenBalance = IERC20(toToken).balanceOf(address(this));
+
+        uint256 boughtAmount = afterTokenBalance - beforeTokenBalance;
+        require(boughtAmount >= minOut, "Too little received");
+
+        _decreaseExposure(tokenManager, CAI, beforeCaiBalance - afterCaiBalance);
+        _increaseExposure(tokenManager, toToken, boughtAmount);
+
+        emit CaiBurned(
+            msg.sender,
+            shares,
+            toTokenSymbol,
+            boughtAmount,
+            block.timestamp
+        );
+    }
+
+    modifier onlyOwner() {
+        DiamondStorageLib.enforceIsContractOwner();
+        _;
+    }
+
+    /* ========== RECEIVE AVAX FUNCTION ========== */
+    receive() external payable {}
+
+    /**
+     * @dev emitted after a CAI mint
+     * @param user the address of user minting the CAI
+     * @param soldAsset sold by the user
+     * @param amountSold amount of tokens sold
+     * @param amountMinted amount of CAI minted
+     * @param timestamp time of the mint
+     **/
+    event CaiMinted(
+        address indexed user,
+        bytes32 indexed soldAsset,
+        uint256 amountSold,
+        uint256 amountMinted,
+        uint256 timestamp
+    );
+
+    /**
+     * @dev emitted after a CAI mint
+     * @param user the address of user minting the CAI
+     * @param shares amount of CAI burned
+     * @param buyAsset redeemed by the user
+     * @param amountRedeemed amount of CAI minted
+     * @param timestamp time of the mint
+     **/
+    event CaiBurned(
+        address indexed user,
+        uint256 indexed shares,
+        bytes32 indexed buyAsset,
+        uint256 amountRedeemed,
+        uint256 timestamp
+    );
+}
--- test/_helpers.ts
+++ test/_helpers.ts
@@ -1,7 +1,11 @@
 import {ethers, network, waffle} from "hardhat";
-import {BigNumber, BigNumberish, Contract, Wallet} from "ethers";
+import { config } from "dotenv";
+import {BigNumber, BigNumberish, Contract, Wallet, constants, providers} from "ethers";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import { TransactionParams } from '@paraswap/sdk';
+import {
+    ZeroExAggregator,
+} from "@phuture/sdk";
 import {
     MockToken,
     Pool,
@@ -42,8 +46,13 @@ export const LPAbi = require('./abis/LP.json');
 export const wavaxAbi = require('./abis/WAVAX.json');
 export const yakRouterAbi = require('./abis/YakRouter.json');
 export const GLPManagerRewarderAbi = require('./abis/GLPManagerRewarder.json');
+export const PhuturePriceOracleAbi = require('./abis/PhuturePriceOracle.json');
+export const PhutureIndexAbi = require('./abis/PhutureIndex.json');
+export const PhutureIndexRouterAbi = require('./abis/PhutureIndexRouter.json');
 
 export const ZERO = ethers.constants.AddressZero;
+
+config();
 
 const token_addresses = {
     avax: addresses,
@@ -897,6 +906,16 @@ export const deployAllFacets = async function (diamondAddress: any, mock: boolea
         ],
         hardhatConfig);
 
+        await deployFacet(
+            "CaiFacet",
+            diamondAddress,
+            [
+                'mintCai',
+                'burnCai',
+            ],
+            hardhatConfig
+        );
+
         if (mock) {
             await deployFacet("UniswapV3FacetMock", diamondAddress, ['mintLiquidityUniswapV3', 'increaseLiquidityUniswapV3', 'decreaseLiquidityUniswapV3', 'burnLiquidityUniswapV3', 'getOwnedUniswapV3TokenIds'], hardhatConfig)
         } else {
@@ -1425,8 +1444,12 @@ export class StakedPosition {
 }
 
 export const parseParaSwapRouteData = (txParams: TransactionParams) => {
-    const selector = txParams.data.slice(0, 10);
-    const data = "0x" + txParams.data.slice(10);
+    return splitCallData(txParams.data);
+};
+
+export const splitCallData = (calldata: string) => {
+    const selector = calldata.slice(0, 10);
+    const data = "0x" + calldata.slice(10);
     return {
         selector,
         data
@@ -1448,4 +1471,298 @@ export const getContractSelectors = (contract: Contract) => {
     });
 }
 
+/**
+ * Retrieves the buy amounts for the given amounts and input token.
+ * @param amounts - An array of objects containing asset and amount.
+ * @param inputToken - The input token.
+ * @returns A promise that resolves to an array of objects containing buy amounts and other details.
+ */
+async function getBuyAmounts(amounts, inputToken) {
+    /// Prepare buy amounts for each asset
+    const buyAmounts = amounts.map(async ({ asset, amount }) => {
+        /// If the asset is the input token or the amount is zero, return the asset and amount
+        if (asset === inputToken || amount.isZero()) {
+            return {
+                asset,
+                swapTarget: constants.AddressZero,
+                buyAssetMinAmount: amount,
+                assetQuote: [],
+            }
+        }
 
+        const [zeroExAggregator] = ZeroExAggregator.fromUrl(
+            "https://avalanche.api.0x.org",
+            43314,
+            process.env.ZERO_EX_API_KEY,
+        );
+
+        /// Otherwise, get the buy amount from the 0x Aggregator
+        const zeroExResult = await zeroExAggregator.quote(inputToken, asset, amount)
+
+        return {
+            asset,
+            swapTarget: zeroExResult.to,
+            buyAssetMinAmount: BigNumber.from(zeroExResult.buyAmount).mul(99).div(100).toString(),
+            assetQuote: zeroExResult.data,
+        }
+    })
+
+    /// Use `Promise.all` to resolve all buy amounts concurrently
+    return await Promise.all(buyAmounts)
+}
+
+/**
+ * Calculates the buy amounts in base currency.
+ * @param buyAmounts - An array of objects containing asset and buy asset min amount.
+ * @param amounts - An array of objects containing asset, amount, and weight.
+ * @returns A promise that resolves to an array of objects containing asset and quoted buy amount.
+ */
+async function calculateBuyAmountsInBase(buyAmounts, amounts) {
+    const priceOracle = new ethers.Contract(
+        "0x69E848b2F41019340CeC3e6696D5c937e74Da96b",
+        PhuturePriceOracleAbi,
+        provider,
+    );
+
+    /// Calculate the buy amounts in base currency
+    const buyAmountsInBase = buyAmounts.map(
+        async ({ asset, buyAssetMinAmount }, amountIndex) => {
+            /// Get the price of the asset in base currency from the PriceOracle
+            /// Note: `callStatic` is used because this is not a view function
+            const price = await priceOracle.callStatic.refreshedAssetPerBaseInUQ(
+                asset,
+            )
+
+            /// Calculate the quoted buy amount in base currency
+            const quotedBuyAmount = BigNumber.from(buyAssetMinAmount)
+                .mul(BigNumber.from(2).pow(112))
+                .mul(255)
+                .div(price.mul(amounts[amountIndex]!.weight))
+
+            return { asset, quotedBuyAmount }
+        },
+    )
+
+    /// Use `Promise.all` to resolve all buy amounts in base currency concurrently
+    return await Promise.all(buyAmountsInBase)
+}
+
+/**
+ * Retrieves quotes for the given amounts, input token, and scaled sell amounts.
+ * @param amounts - An array of objects containing asset and amount.
+ * @param inputToken - The input token.
+ * @param scaledSellAmounts - An array of scaled sell amounts.
+ * @returns A promise that resolves to an array of quotes for the given amounts.
+ */
+async function getMintQuotes(amounts, inputToken, scaledSellAmounts) {
+    /// Prepare quotes for each asset
+    const quotes = amounts.map(async ({ asset }, i) => {
+        const scaledSellAmount = scaledSellAmounts[i] as BigNumber
+
+        /// If the asset is the input token or the scaled sell amount is zero, return the asset and amount
+        if (asset === inputToken || scaledSellAmount.isZero()) {
+            return {
+            asset,
+            swapTarget: constants.AddressZero,
+            buyAssetMinAmount: scaledSellAmounts[i]!,
+            assetQuote: [],
+            }
+        }
+
+        const [zeroExAggregator] = ZeroExAggregator.fromUrl(
+            "https://avalanche.api.0x.org",
+            43314,
+            process.env.ZERO_EX_API_KEY || "",
+        );
+
+        /// Otherwise, get the quote from the 0x Aggregator
+        const zeroExResult = await zeroExAggregator.quote(
+            inputToken,
+            asset,
+            scaledSellAmount,
+        )
+
+        return {
+            asset,
+            buyAssetMinAmount: BigNumber.from(zeroExResult.buyAmount).mul(99).div(100),
+            swapTarget: zeroExResult.to,
+            assetQuote: zeroExResult.data,
+        }
+    })
+
+    /// Use `Promise.all` to resolve all quotes concurrently
+    return await Promise.all(quotes)
+}
+
+/**
+ * Prepares quotes for minting tokens.
+ * @param inputToken - The input token for the mint operation.
+ * @param amountIn - The amount of input token to mint.
+ * @returns A promise that resolves to an object containing quotes and the total sell amount.
+ */
+async function prepareMintQuotes(inputToken, amountIn) {
+    const index = new ethers.Contract(
+        "0x48f88A3fE843ccb0b5003e70B4192c1d7448bEf0",
+        PhutureIndexAbi,
+        provider,
+    );
+
+    /// Retrieve the current index anatomy and inactive anatomy from the index contract
+    const { _assets, _weights } = await index.anatomy()
+
+    /// Prepare amounts for each asset
+    const amounts = _assets.map((asset, i) => ({
+        asset,
+        amount: BigNumber.from(amountIn).mul(_weights[i]).div(255),
+        weight: _weights[i],
+    }))
+
+    /// Get the buy amounts for the given amounts and input token
+    const buyAmounts = await getBuyAmounts(amounts, inputToken)
+
+    /// Calculate the buy amounts in base currency
+    const buyAmountsInBase = await calculateBuyAmountsInBase(buyAmounts, amounts)
+
+    /// Find the minimum buy amount in base currency
+    const minBuyAmount = buyAmountsInBase.reduce((min, curr) =>
+        min.quotedBuyAmount.lte(curr.quotedBuyAmount) ? min : curr,
+    )
+
+    /// Scale the sell amounts based on the minimum buy amount
+    const scaledSellAmounts: any[] = Object.values(amounts).map(({ amount }, i) =>
+        amount
+            .mul(minBuyAmount.quotedBuyAmount)
+            .div(buyAmountsInBase[i].quotedBuyAmount),
+    )
+
+    /// Get quotes for the given amounts, input token, and scaled sell amounts
+    const quotes = await getMintQuotes(amounts, inputToken, scaledSellAmounts)
+
+    /// Calculate the total sell amount
+    const sellAmount = scaledSellAmounts.reduce(
+        (sum, curr) => sum.add(curr),
+        BigNumber.from(0),
+    )
+
+    return { quotes, sellAmount }
+}
+
+/**
+ * Prepares quotes for minting tokens.
+ * @param inputToken - The input token for the mint operation.
+ * @param amountIn - The amount of input token to mint.
+ * @returns A promise that resolves to an object containing quotes and the total sell amount.
+ */
+export async function getMintData(inputToken, amountIn, recipient) {
+    /// Prepare quotes for minting tokens and get the scaled total sell amount
+    const { quotes, sellAmount } = await prepareMintQuotes(inputToken, amountIn)
+
+    const indexRouter = new ethers.Contract("0xD6dd95610fC3A3579a2C32fe06158d8bfB8F4eE9", PhutureIndexRouterAbi, provider)
+
+    // Use `.mintSwapValue` if you want to use native currency as input
+    const mintTx = await indexRouter.populateTransaction.mintSwap({
+        index: "0x48f88A3fE843ccb0b5003e70B4192c1d7448bEf0",
+        inputToken: inputToken,
+        recipient,
+        amountInInputToken: sellAmount,
+        quotes,
+    });
+
+    return splitCallData(mintTx.data);
+}
+
+/**
+ * Prepares quotes for burning tokens.
+ *
+ * @param shares - The number of shares to burn.
+ * @param outputToken - The output token for the burn operation.
+ * @param recipient - The recipient address
+ * @returns A promise that resolves to an array of quotes for burning tokens.
+ */
+async function prepareBurnQuotes(shares, outputToken, recipient) {
+    const index = new ethers.Contract(
+        "0x48f88A3fE843ccb0b5003e70B4192c1d7448bEf0",
+        PhutureIndexAbi,
+        provider,
+    );
+    const indexRouter = new ethers.Contract(
+        "0xD6dd95610fC3A3579a2C32fe06158d8bfB8F4eE9",
+        PhutureIndexRouterAbi,
+        provider
+    );
+
+    /// Retrieve the current index anatomy and inactive anatomy from the index contract
+    const [{ _assets, _weights }, inactiveAnatomy, burnTokensAmounts] =
+        await Promise.all([
+            index.anatomy(),
+            index.inactiveAnatomy(),
+            indexRouter.callStatic.burnWithAmounts(
+                {
+                    index: index.address,
+                    recipient,
+                    amount: shares,
+                },
+                {
+                    from: recipient,
+                },
+            ),
+        ])
+
+    /// Merge the active and inactive anatomy into a single array
+    const constituents = [
+        ..._assets.map((asset, i) => ({ asset, weight: _weights[i] })),
+        ...inactiveAnatomy.map((asset) => ({ asset, weight: 0 })),
+    ]
+
+    /// Prepare quotes for burning tokens
+    const quotes = constituents.map(async ({ asset }, constituentIndex) => {
+        const amount = burnTokensAmounts[constituentIndex] || BigNumber.from(0)
+        /// If the amount is zero or the asset is the output token, return an empty quote
+        if (!amount || amount.isZero() || asset === outputToken) {
+            return {
+                swapTarget: constants.AddressZero,
+                assetQuote: [],
+                buyAssetMinAmount: 0,
+            }
+        }
+
+        const [zeroExAggregator] = ZeroExAggregator.fromUrl(
+            "https://avalanche.api.0x.org",
+            43314,
+            process.env.ZERO_EX_API_KEY,
+        );
+        /// Use the 0x Aggregator to get a quote for burning the token
+        const zeroExResult = await zeroExAggregator.quote(
+            asset,
+            outputToken,
+            amount.mul(98).div(100),
+        )
+
+        return {
+            swapTarget: zeroExResult.to,
+            buyAssetMinAmount: zeroExResult.buyAmount,
+            assetQuote: zeroExResult.data,
+        }
+    })
+
+    /// Use `Promise.all` to resolve all the quotes concurrently
+    return await Promise.all(quotes)
+}
+
+export async function getBurnData(shares, outputToken, recipient) {
+    /// Prepare quotes for burning tokens
+    const quotes = await prepareBurnQuotes(shares, outputToken, recipient);
+
+    const indexRouter = new ethers.Contract("0xD6dd95610fC3A3579a2C32fe06158d8bfB8F4eE9", PhutureIndexRouterAbi, provider);
+
+    // Use `.burnSwapValue` if you want to use native currency as output
+    const burnTx =  await indexRouter.populateTransaction.burnSwap({
+        index: "0x48f88A3fE843ccb0b5003e70B4192c1d7448bEf0",
+        amount: shares,
+        outputAsset: outputToken,
+        recipient: recipient,
+        quotes,
+    });
+
+    return splitCallData(burnTx.data);
+}
