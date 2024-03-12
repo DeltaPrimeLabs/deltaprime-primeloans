6607e46185e84d2dfcfb53d6d4dbf6ae3e8614ad
kavsky02 <kamilovsky@deltaprime.io>
2024-03-12 16:53:55 +0100

    Fix tjv2 positions in HealthMeterFacetProd.sol

--- contracts/facets/HealthMeterFacetProd.sol
+++ contracts/facets/HealthMeterFacetProd.sol
@@ -5,21 +5,96 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@redstone-finance/evm-connector/contracts/data-services/ArbitrumProdDataServiceConsumerBase.sol";
 import "../interfaces/ITokenManager.sol";
+import "../interfaces/facets/avalanche/ITraderJoeV2Facet.sol";
+import {Uint256x256Math} from "../lib/joe-v2/math/Uint256x256Math.sol";
+import {PriceHelper} from "../lib/joe-v2/PriceHelper.sol";
 import "../Pool.sol";
 
 //This path is updated during deployment
 import "../lib/local/DeploymentConstants.sol";
 
 contract HealthMeterFacetProd is ArbitrumProdDataServiceConsumerBase {
+    using PriceHelper for uint256;
+    using Uint256x256Math for uint256;
+
     struct AssetPrice {
         bytes32 asset;
         uint256 price;
     }
 
+    struct PriceInfo {
+        address tokenX;
+        address tokenY;
+        uint256 priceX;
+        uint256 priceY;
+    }
+
+    function _getTotalTraderJoeV2Weighted() internal view returns (uint256) {
+        uint256 total;
+
+        ITraderJoeV2Facet.TraderJoeV2Bin[] memory ownedTraderJoeV2Bins = DiamondStorageLib.getTjV2OwnedBinsView();
+
+        PriceInfo memory priceInfo;
+
+        if (ownedTraderJoeV2Bins.length > 0) {
+            for (uint256 i; i < ownedTraderJoeV2Bins.length; i++) {
+                ITraderJoeV2Facet.TraderJoeV2Bin memory binInfo = ownedTraderJoeV2Bins[i];
+
+                uint256 price;
+                uint256 liquidity;
+
+                {
+                    address tokenXAddress = address(binInfo.pair.getTokenX());
+                    address tokenYAddress = address(binInfo.pair.getTokenY());
+
+                    if (priceInfo.tokenX != tokenXAddress || priceInfo.tokenY != tokenYAddress) {
+                        bytes32[] memory symbols = new bytes32[](2);
+
+
+                        symbols[0] = DeploymentConstants.getTokenManager().tokenAddressToSymbol(tokenXAddress);
+                        symbols[1] = DeploymentConstants.getTokenManager().tokenAddressToSymbol(tokenYAddress);
+
+                        uint256[] memory prices = getOracleNumericValuesFromTxMsg(symbols);
+                        priceInfo = PriceInfo(tokenXAddress, tokenYAddress, prices[0], prices[1]);
+                    }
+                }
+
+                {
+                    (uint128 binReserveX, uint128 binReserveY) = binInfo.pair.getBin(binInfo.id);
+
+                    price = PriceHelper.convert128x128PriceToDecimal(binInfo.pair.getPriceFromId(binInfo.id)); // how is it denominated (what precision)?
+
+                    liquidity = price * binReserveX / 10 ** 18 + binReserveY;
+                }
+
+
+                {
+                    uint256 debtCoverageX = DeploymentConstants.getTokenManager().debtCoverage(address(binInfo.pair.getTokenX()));
+                    uint256 debtCoverageY = DeploymentConstants.getTokenManager().debtCoverage(address(binInfo.pair.getTokenY()));
+
+                    total = total +
+                                                        Math.min(
+                                            price > 10 ** 24 ?
+                                                debtCoverageX * liquidity / (price / 10 ** 18) / 10 ** IERC20Metadata(address(binInfo.pair.getTokenX())).decimals() * priceInfo.priceX / 10 ** 8
+                                                :
+                                                debtCoverageX * liquidity / price * 10 ** 18 / 10 ** IERC20Metadata(address(binInfo.pair.getTokenX())).decimals() * priceInfo.priceX / 10 ** 8,
+                                            debtCoverageY * liquidity / 10 ** (IERC20Metadata(address(binInfo.pair.getTokenY())).decimals()) * priceInfo.priceY / 10 ** 8
+                                        )
+                                        .mulDivRoundDown(binInfo.pair.balanceOf(address(this), binInfo.id), 1e18)
+                                .mulDivRoundDown(1e18, binInfo.pair.totalSupply(binInfo.id));
+                }
+            }
+
+            return total;
+        } else {
+            return 0;
+        }
+    }
+
     /**
       * Returns an array of bytes32[] symbols of debt (borrowable) assets.
     **/
-    function _getDebtAssets() internal view returns(bytes32[] memory result) {
+    function _getDebtAssets() internal view returns (bytes32[] memory result) {
         ITokenManager tokenManager = DeploymentConstants.getTokenManager();
         result = tokenManager.getAllPoolAssets();
     }
@@ -28,13 +103,13 @@ contract HealthMeterFacetProd is ArbitrumProdDataServiceConsumerBase {
       * Returns an array of Asset/Price structs of enriched (always containing AVAX at index 0) owned assets.
       * @dev This function uses the redstone-evm-connector
     **/
-    function _getOwnedAssetsWithNativePrices() internal view returns(AssetPrice[] memory result) {
+    function _getOwnedAssetsWithNativePrices() internal view returns (AssetPrice[] memory result) {
         bytes32[] memory assetsEnriched = _getOwnedAssetsWithNative();
         uint256[] memory prices = getOracleNumericValuesFromTxMsg(assetsEnriched);
 
         result = new AssetPrice[](assetsEnriched.length);
 
-        for(uint i; i<assetsEnriched.length; i++){
+        for (uint i; i < assetsEnriched.length; i++) {
             result[i] = AssetPrice({
                 asset: assetsEnriched[i],
                 price: prices[i]
@@ -45,7 +120,7 @@ contract HealthMeterFacetProd is ArbitrumProdDataServiceConsumerBase {
     /**
       * Returns list of owned assets that always included NativeToken at index 0
     **/
-    function _getOwnedAssetsWithNative() internal view returns(bytes32[] memory){
+    function _getOwnedAssetsWithNative() internal view returns (bytes32[] memory){
         bytes32[] memory ownedAssets = DeploymentConstants.getAllOwnedAssets();
         bytes32 nativeTokenSymbol = DeploymentConstants.getNativeTokenSymbol();
 
@@ -56,8 +131,8 @@ contract HealthMeterFacetProd is ArbitrumProdDataServiceConsumerBase {
         uint256 lastUsedIndex;
         assetsWithNative[0] = nativeTokenSymbol; // First asset = NativeToken
 
-        for(uint i=0; i< ownedAssets.length; i++){
-            if(ownedAssets[i] != nativeTokenSymbol){
+        for (uint i = 0; i < ownedAssets.length; i++) {
+            if (ownedAssets[i] != nativeTokenSymbol) {
                 lastUsedIndex += 1;
                 assetsWithNative[lastUsedIndex] = ownedAssets[i];
             }
@@ -80,6 +155,8 @@ contract HealthMeterFacetProd is ArbitrumProdDataServiceConsumerBase {
         uint256 weightedCollateralMinus = 0;
         uint256 weightedBorrowed = 0;
         uint256 borrowed = 0;
+
+        weightedCollateralPlus += _getTotalTraderJoeV2Weighted();
 
         for (uint256 i = 0; i < ownedAssetsPrices.length; i++) {
             IERC20Metadata token = IERC20Metadata(tokenManager.getAssetAddress(ownedAssetsPrices[i].asset, true));
