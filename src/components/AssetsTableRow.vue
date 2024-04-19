a8b097c9dd05c692ebc1ff727294b3853eb247e1
LeCh1ffre <lechiffre@deltaprime.io>
2024-04-19 18:41:34 +0200

    UI: change drop support date for 0x assets

--- src/components/AssetsTableRow.vue
+++ src/components/AssetsTableRow.vue
@@ -9,7 +9,7 @@
             {{ asset.symbol }}
             <img v-if="asset.droppingSupport && assetBalances[asset.symbol] > 0"
                  src="src/assets/icons/warning.svg"
-                 v-tooltip="{content: `We will drop support to this asset on ${ asset.debtCoverage > 0.1 ? '26.04.2024 12:00 CET' : '19.04.2024 12:00 CET'}. Please withdraw or swap to another token.`, classes: 'info-tooltip long'}">
+                 v-tooltip="{content: `We will drop support to this asset on ${ asset.debtCoverage > 0.1 ? '26.04.2024 12:00 CET' : 'Monday 22.04.2024 16:00 CET'}. Please withdraw or swap to another token.`, classes: 'info-tooltip long'}">
           </div>
           <div class="asset__loan" v-if="borrowApyPerPool && borrowApyPerPool[asset.symbol] !== undefined">
             Borrow&nbsp;APY:&nbsp;{{ borrowApyPerPool[asset.symbol] | percent }}
