aa22e822ea95c80c820b98d87b914ae95dce976f
0xlock <0xl0ck@proton.me>
2024-04-30 09:17:15 -0500

    use .safeTransfer for token transfer

--- contracts/facets/arbitrum/PenpieFacet.sol
+++ contracts/facets/arbitrum/PenpieFacet.sol
@@ -181,7 +181,7 @@ contract PenpieFacet is ReentrancyGuardKeccak, OnlyOwnerOrInsolvent {
 
             uint256 pnpReceived = IERC20(PNP).balanceOf(address(this));
             if (pnpReceived > 0) {
-                IERC20(PNP).transfer(msg.sender, pnpReceived);
+                PNP.safeTransfer(msg.sender, pnpReceived);
             }
 
             _increaseExposure(tokenManager, token, amount);
