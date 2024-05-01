952c54450a949406a53127165a99d6560f024b8c
kavsky02 <kamilovsky@deltaprime.io>
2024-05-01 13:44:41 +0200

    Add SolvencyFacetProdArbitrum.sol deployment files

--- contracts/facets/arbitrum/SolvencyFacetProdArbitrum.sol
+++ contracts/facets/arbitrum/SolvencyFacetProdArbitrum.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: d0e8ed51430900dbfc0d3920039874c8fd080b08;
+// Last deployed from commit: 1b0ba8bbc31f6a31a48e3ec0e05c7d6ad96a8c12;
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
