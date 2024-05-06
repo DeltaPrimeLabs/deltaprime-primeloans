596e178dd45f6beee4ad8bebcc4040a3f5378bf8
keizir <ultimatesofteng.work@gmail.com>
2024-05-06 14:26:00 +0000

    Lambda: ltip boost api and make bash script executable

--- lambda/apis/arbitrumIncentives.js
+++ lambda/apis/arbitrumIncentives.js
@@ -56,6 +56,30 @@ const getLtipBoostApyApi = (event, context, callback) => {
     });
 };
 
+const getLtipBoostApyApi = (event, context, callback) => {
+  const params = {
+    TableName: process.env.APY_TABLE,
+    Key: {
+      id: "LTIP_BOOST"
+    }
+  };
+
+  dynamoDb.get(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch GM Boost APY.'));
+      return;
+    });
+};
+
 module.exports = {
-  getArbitrumIncentivesApi
+  getArbitrumIncentivesApi,
+  getLtipBoostApyApi
 }
