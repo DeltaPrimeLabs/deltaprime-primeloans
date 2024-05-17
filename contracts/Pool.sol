3f9a3944bd7b3301b9ed697a107d561601a89cfb
kavsky02 <kamilovsky@deltaprime.io>
2024-05-17 20:22:46 +0200

    Add docstring for Pool::getFullyVestedLockedBalance

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -92,6 +92,14 @@ contract Pool is OwnableUpgradeable, ReentrancyGuardUpgradeable, IERC20, ProxyCo
     }
 
 
+    /**
+     * @notice Calculates and returns the fully vested locked balance for a given account.
+     * @dev The fully vested locked balance is used in the governance mechanism of the system, specifically for the allocation of vPrime tokens.
+     * The method calculates the fully vested locked balance by iterating over all the locks of the account and summing up the amounts of those locks that are still active (i.e., their `unlockTime` is greater than the current block timestamp). However, the amount of each lock is scaled by the ratio of its `lockTime` to the `MAX_LOCK_TIME` (3 years). This means that the longer the lock time, the larger the contribution of the lock to the fully vested locked balance.
+     * The fully vested locked balance is used to calculate the maximum vPrime allocation for a user. Users accrue vPrime over a period of 3 years, from 0 to the maximum vPrime based on their 10-1 pairs of pool-deposit and sPrime. Locking pool deposits and sPrime immediately vests the vPrime.
+     * @param account The address of the account for which to calculate the fully vested locked balance.
+     * @return fullyVestedBalance The fully vested locked balance of the provided account.
+     */
     function getFullyVestedLockedBalance(address account) public view returns (uint256 fullyVestedBalance) {
         fullyVestedBalance = 0;
         for (uint i = 0; i < locks[account].length; i++) {
