9b4484e0578739dc83204911b15806022c42bb71
kavsky02 <kamilovsky@deltaprime.io>
2024-05-19 23:33:11 +0200

    Fix pragma in PendingOwnableUpgradeable.sol

--- contracts/abstract/PendingOwnableUpgradeable.sol
+++ contracts/abstract/PendingOwnableUpgradeable.sol
@@ -2,9 +2,6 @@
 // Last deployed from commit: ;
 pragma solidity 0.8.17;
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "../interfaces/IPendingOwnableUpgradeable.sol";
 
