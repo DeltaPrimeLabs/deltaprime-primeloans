86622fadeb55b6b31e8adb742bf9230cfff782a0
keizir <ultimatesofteng.work@gmail.com>
2024-05-28 22:45:01 +0800

    Lambda: lambda in eu region

---
+++ lambda-eu/aggregators/apy.js
@@ -0,0 +1,73 @@
+
+const { newChrome } = require("../utils/chrome");
+const { dynamoDb } = require("../utils/helpers");
+const wombatApyConfig = require("../config/wombatApy.json");
+
+const wombatApyAggregator = async (event) => {
+  const { page } = await newChrome();
+
+  // navigate pools page and wait till javascript fully load.
+  const BASE_URL = "https://app.wombat.exchange/pool";
+
+  await page.setViewport({
+    width: 1920,
+    height: 1080
+  });
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+
+  // fetch GM tokens' APYs on Arbitrum and Avalanche
+  for (const [network, pools] of Object.entries(wombatApyConfig)) {
+    const PAGE_URL = `${BASE_URL}?chain=${network}`;
+    await page.goto(PAGE_URL, {
+      waitUntil: "networkidle0",
+      timeout: 60000
+    });
+
+    const poolRows = await page.$$("#pool-list > div.flex-col > div.flex-col > div.flex-col > div");
+    const poolInnerTexts = await Promise.all(Array.from(poolRows).map(async pool => {
+      return (await (await pool.getProperty("textContent")).jsonValue()).replace(/\s+/g, "");
+    }));
+    // console.log(poolInnerTexts)
+
+    for (const [identifier, index] of Object.entries(pools)) {
+      try {
+        const pool = poolRows[index];
+        const tvlColumn = await pool.$("div > div.relative > div.items-center > div.justify-center > div.relative > span");
+        const tvlRaw = (await (await tvlColumn.getProperty("textContent")).jsonValue()).replace(/\s+/g, "").replace('$', '').trim();
+        const tvlUnit = tvlRaw.charAt(tvlRaw.length - 1) === "K" ? 1000 : 1000000;
+        const poolTvl = tvlUnit * parseFloat(tvlRaw.slice(0, -1));
+
+        // const poolApy = poolInnerTexts[index].split('Avg.APR')[1].split('AverageTotalAPR')[0].replace('%', '').trim();
+        const apyColumn = await pool.$("div > div.relative > div.items-center > div.justify-self-center > p");
+        const poolApy = (await (await apyColumn.getProperty("textContent")).jsonValue()).replace(/\s+/g, "").replace('%', '').trim();
+
+        console.log(identifier, poolTvl, poolApy);
+
+        const params = {
+          TableName: process.env.APY_TABLE,
+          Key: {
+            id: identifier
+          },
+          AttributeUpdates: {
+            lp_apy: {
+              Value: Number(poolApy) ? poolApy / 100 : null,
+              Action: "PUT"
+            }
+          }
+        };
+        await dynamoDb.update(params).promise();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  console.log('fetching done.');
+
+  return event;
+}
+
+module.exports = {
+  wombatApyAggregator
+}
