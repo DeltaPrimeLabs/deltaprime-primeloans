02b659476c6874611b946f4897cbdab8e22cc98e
mbare <mbare@deltaprime.io>
2024-05-29 16:18:36 +0200

    UI: fetch gnosis transactions

--- tools/scripts/fetch-gnosis-safe-txs.js
+++ tools/scripts/fetch-gnosis-safe-txs.js
@@ -1,11 +1,13 @@
-const NETWORK_SLUG = "avalanche";
-const SAFE_ADDRESS = "0x44AfCcF712E8A097a6727B48b57c75d7A85a9B0c";
-const NATIVE_TOKEN_SLUG = "AVAX";
-const TOKEN_ADDRESSES = require('./token_addresses_for_scripts.json');
+const NETWORK_SLUG = "arbitrum";
+const SAFE_ADDRESS = "0x764a9756994f4E6cd9358a6FcD924d566fC2e666";
+// const SAFE_ADDRESS = "0x18C244c62372dF1b933CD455769f9B4DdB820F0C";
+const NATIVE_TOKEN_SLUG = "ETH";
+const TOKEN_ADDRESSES = require('./token_addresses_for_scripts_arbitrum.json');
 const TOKEN_DECIMALS = require('./token_decimals_for_scripts.json');
 
 const fetch = require("node-fetch");
 const fs = require("fs");
+const {formatUnits} = require("./fetch-parse-event-for-accounts");
 
 
 function arrayToCSV(data) {
@@ -18,15 +20,12 @@ function arrayToCSV(data) {
     return csvRows.join('\n');
 }
 
-const run = async () => {
+const fetchMultisigTransactions = async () => {
     let transactions = [];
     let i = 0;
     let resp;
 
     while (i == 0 || resp.next) {
-        if (resp){
-            console.log(resp.next)
-        }
         let url =
             i == 0
             ?
@@ -36,7 +35,6 @@ const run = async () => {
 
         resp = await (await fetch(url)).json();
         let filteredResults = resp.results.filter(tx => tx.transactionHash && (Number(tx.value) > 0 || (tx.dataDecoded && tx.dataDecoded.method === 'transfer')));
-        console.log(filteredResults.map(el => el.dataDecoded ? el.dataDecoded.parameters : null))
         transactions.push(...filteredResults.map(el => {
                 let isERC20 =  Number(el.value) == 0 && el.dataDecoded && el.dataDecoded.method === 'transfer';
                 let tokenAddress = isERC20 ? el.to : null;
@@ -61,6 +59,92 @@ const run = async () => {
 
 }
 
+const fetchReceivedTransfers = async () => {
+    let transactions = [];
+    let i = 0;
+    let resp;
 
+    while (i == 0 || resp.next) {
+        if (resp){
+            console.log(resp.next)
+        }
+        let url =
+            i == 0
+                ?
+                `https://safe-transaction-${NETWORK_SLUG}.safe.global/api/v1/safes/${SAFE_ADDRESS}/incoming-transfers/?limit=200`
+                :
+                resp.next;
 
-run()
+        resp = await (await fetch(url)).json();
+        let filteredResults = resp.results.filter(el => new Date(el.executionDate).getTime() < 1708732800000)
+        // let filteredResults = resp.results;
+
+        transactions.push(...filteredResults.map(el => {
+            let amount, tokenSymbol;
+
+            if (!el.tokenInfo) return [];
+            let info = Object.entries(TOKEN_ADDRESSES).find(([k,v]) => v.toLowerCase() === el.tokenInfo.address.toLowerCase());
+            if (info) tokenSymbol = info[0];
+
+            if (tokenSymbol) {
+                amount = el.value / 10**el.tokenInfo.decimals;
+                console.log(new Date(el.executionDate).getTime()  / 1000)
+                let dollarValue = getDollarValue(tokenSymbol, amount, new Date(el.executionDate).getTime()  / 1000);
+                return [el.executionDate, el.transactionHash, tokenSymbol, el.tokenInfo.address, amount, dollarValue]
+            } else {
+                console.log(el)
+                return []
+            }
+
+        }));
+        i++;
+    }
+
+    const csvPath = `gnosis-received-transfers-${NETWORK_SLUG}-${SAFE_ADDRESS}.csv`; // Path where you want to save the CSV file
+    const csvData = arrayToCSV(transactions);
+
+    fs.writeFile(csvPath, csvData, (err) => {
+        if (err) throw err;
+        console.log('CSV file has been saved.');
+    });
+
+}
+
+function getDollarValue(symbol, amount, timestamp) {
+    const prices = JSON.parse(fs.readFileSync('historical_prices_array.json')).list;
+    prices.sort((a, b) => a[0] - b[0]);
+
+    let feed;
+    let feedTimestamp;
+    let noFeedAtTimestamp = false;
+    let i = 1;
+
+    while (!feed && i < prices.length) {
+        if ((parseInt(prices[i - 1][0]) > timestamp && timestamp < parseInt(prices[i][0])) ||
+            noFeedAtTimestamp ||
+            i === prices.length - 1
+        ) {
+            feed = prices[i - 1][1].find(feed => feed.dataFeedId === symbol);
+
+            if (!feed) {
+                noFeedAtTimestamp = true;
+            } else {
+                feedTimestamp = prices[i - 1][0];
+            }
+        }
+
+        i++;
+    }
+
+    if (!feed) {
+        console.log(`Missing feed for ${symbol}, timestamp: ${feedTimestamp}`)
+        return 0;
+    } else {
+        console.log(`${feedTimestamp} feed: ${new Date(feedTimestamp * 1000)}  & ${new Date(timestamp * 1000)}`)
+        console.log(`Symbol: ${symbol}, time: ${new Date(feedTimestamp * 1000)}, price: ${feed.value}`)
+        return feed.value * amount;
+    }
+}
+
+// fetchReceivedTransfers()
+fetchMultisigTransactions()
