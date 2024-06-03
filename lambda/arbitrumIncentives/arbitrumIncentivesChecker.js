8304d9859b97d61ea6370a73c0753622c509a028
kavsky02 <kamilovsky@deltaprime.io>
2024-06-03 21:25:24 +0200

    Update LTIP::PrimeAcounts weekly ARB rate

--- lambda/arbitrumIncentives/arbitrumIncentivesChecker.js
+++ lambda/arbitrumIncentives/arbitrumIncentivesChecker.js
@@ -5,8 +5,8 @@ const {
 const pingUrl = require('../.secrets/ping.json');
 
 const threshold = 0.000001;
-// 50.32419 = 8454.4642857 ARB per week hourly
-const expectedIncentives = 50.32419; // change the value accordingly based on incentives of interval
+// 50.324192176785714 = 8454.4642857 ARB per week hourly
+const expectedIncentives = 50.324192176785714; // change the value accordingly based on incentives of interval
 
 const arbitrumIncentivesChecker = async () => {
   try {
