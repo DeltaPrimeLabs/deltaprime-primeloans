d7f9f4970c69a356feb609fe4920bfa7cb48ce0a
kavsky02 <kamilovsky@deltaprime.io>
2024-06-04 13:24:53 +0200

    Revert "fix: uniswap v3 library integration for solvency facet prod"
    This reverts commit 6776c123a5f438407780524a53e98a65fee3812c.

--- contracts/interfaces/facets/avalanche/IUniswapV3Facet.sol
+++ contracts/interfaces/facets/avalanche/IUniswapV3Facet.sol
@@ -9,7 +9,6 @@ interface IUniswapV3Facet {
     struct UniswapV3Position {
         address token0;
         address token1;
-        uint24 fee;
         int24 tickLower;
         int24 tickUpper;
         uint128 liquidity;
