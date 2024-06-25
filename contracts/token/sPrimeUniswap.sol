63ae3169f8361a1450465529a761f45cf34aafa4
kavsky02 <kamilovsky@deltaprime.io>
2024-06-25 13:09:12 +0200

    Remove unused imports from sPrimeUniswap.sol

--- contracts/token/sPrimeUniswap.sol
+++ contracts/token/sPrimeUniswap.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.17;
 
 // Importing necessary libraries and interfaces
 import "../interfaces/ISPrimeUniswap.sol";
-import "../interfaces/ITokenManager.sol";
 import "../interfaces/uniswap-v3/IUniswapV3Factory.sol";
 import "../interfaces/uniswap-v3/ISwapRouter.sol";
 import "../lib/uniswap-v3/OracleLibrary.sol";
 import "../lib/uniswap-v3/PositionValue.sol";
 import "../lib/uniswap-v3/UniswapV3IntegrationHelper.sol";
-import "../lib/local/DeploymentConstants.sol";
 import "../abstract/PendingOwnableUpgradeable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
