84497b01716b53fb360c04d763183339d5599174
kavsky02 <kamilovsky@deltaprime.io>
2024-06-27 16:20:55 +0200

    Add deployment files

--- contracts/token/vPrime.sol
+++ contracts/token/vPrime.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: ;
+// Last deployed from commit: c01f75e2bf34101db273d19406f08014faf1e524;
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/utils/math/Math.sol";
--- contracts/token/vPrimeControllerArbitrum.sol
+++ contracts/token/vPrimeControllerArbitrum.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: a14baf0217af123476fa2b7c928bf95c314f3d72;
+// Last deployed from commit: c01f75e2bf34101db273d19406f08014faf1e524;
 pragma solidity 0.8.17;
 
 import "./vPrimeController.sol";
