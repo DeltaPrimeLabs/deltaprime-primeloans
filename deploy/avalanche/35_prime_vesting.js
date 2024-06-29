8ce7ef7b775c75f60e36d4d66915788221e9e8c4
kavsky02 <kamilovsky@deltaprime.io>
2024-06-29 18:36:08 +0200

    Final changes to PrimeVesting contract, test and scripts

--- deploy/avalanche/35_prime_vesting.js
+++ deploy/avalanche/35_prime_vesting.js
@@ -3,65 +3,85 @@ import { embedCommitHash } from "../../tools/scripts/embed-commit-hash";
 const { ethers } = require("hardhat");
 import verifyContract from "../../tools/scripts/verify-contract";
 import hre from "hardhat";
+import fs from "fs";
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
+
+async function readJsonFile(filename) {
+    try {
+        // Read the JSON file asynchronously
+        const data = await readFileAsync(filename, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {}
+}
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
+    const { deployer, admin } = await getNamedAccounts();
 
     embedCommitHash("PrimeVesting", "./contracts");
 
-    const args = [
-        "0x8B1e2420e0d453a718d4b70e3a043263Eab77851",
-        1719522000,
-        [
-            "0x003E3336764DE10a898916B84C626B69D7294339",
-            "0xFC078F7ea5c7648CD78E0cA40799B6d2090f83Dd",
-            "0x72bf67eCBaaB144a84Fb4a81f011Cd27b3cF05a1",
-            "0xE214fba86BeED196AaC68934bF4822C1Cff6B0b8"
-        ],
-        [
-            {
-                cliffPeriod: 6 * 3600,
-                vestingPeriod: 48 * 3600,
-                grantClaimRightTo: ethers.constants.AddressZero,
-                totalAmount: ethers.utils.parseEther("999"),
-            },
-            {
-                cliffPeriod: 0,
-                vestingPeriod: 72 * 3600,
-                grantClaimRightTo: "0x83C3c4B58b70067BFb23B7875FEb63241F9e7668",
-                totalAmount: ethers.utils.parseEther("12345"),
-            },
-            {
-                cliffPeriod: 2 * 3600,
-                vestingPeriod: 48 * 3600,
-                grantClaimRightTo: ethers.constants.AddressZero,
-                totalAmount: ethers.utils.parseEther("40000"),
-            },
-            {
-                cliffPeriod: 8 * 3600,
-                vestingPeriod: 72 * 3600,
-                grantClaimRightTo: "0xC29ee4509F01e3534307645Fc62F30Da3Ec65751",
-                totalAmount: ethers.utils.parseEther("2137"),
-            },
-        ]
-    ];
-    const PrimeVesting = await deploy("PrimeVesting", {
-        from: deployer,
-        args,
+    let participants = await readJsonFile('deploy/avalanche/data/primeVestingData.json');
+
+    const participantsAddresses = participants.map(participant => participant.walletAddress);
+    const vestingInfos = participants.map(participant => {
+        return {
+            cliffPeriod: participant.cliffInSeconds,
+            vestingPeriod: participant.vestingInSeconds,
+            grantClaimRightTo: participant.grantClaimRightTo === null ? ethers.constants.AddressZero : participant.grantClaimRightTo,
+            totalAmount: ethers.utils.parseEther(participant.primeAmount.toString())
+        };
     });
 
+    const PRIME_TOKEN_ADDRESS_AVALANCHE = "0x33C8036E99082B0C395374832FECF70c42C7F298";
+    const START_TIME = 1719853200; // 2024-07-01 19:00:00 CET
+    const _args = [
+        PRIME_TOKEN_ADDRESS_AVALANCHE,
+        START_TIME
+    ]
+
+    let PrimeVesting = await deploy("PrimeVesting", {
+        from: deployer,
+        gasLimit: 15000000,
+        args: _args,
+    });
+
+
     console.log(
-        `PrimeVesting deployed at address: ${PrimeVesting.address}`
+        `PrimeVesting implementation deployed at address: ${PrimeVesting.address}`
     );
 
     await verifyContract(hre,
         {
             address: PrimeVesting.address,
             contract: `contracts/PrimeVesting.sol:PrimeVesting`,
-            constructorArguments: args
+            constructorArguments: _args
         });
     console.log(`Verified PrimeVesting`);
+
+
+    let primeVestingContract = new ethers.Contract(PrimeVesting.address, (await hre.artifacts.readArtifact("PrimeVesting")).abi, hre.ethers.provider.getSigner(deployer));
+
+    const batchSize = 100;
+    const useBatch = false;
+
+    if(useBatch) {
+        for (let i = 0; i < vestingInfos.length; i += batchSize) {
+            const batch = vestingInfos.slice(i, i + batchSize);
+            const isLastBatch = i + batchSize >= vestingInfos.length;
+            console.log(`Initializing vesting contract with participants from ${i} to ${i + batch.length - 1}`);
+            await primeVestingContract.initializeVesting(participantsAddresses.slice(i, i + batchSize), batch, isLastBatch);
+            // sleep 10 seconds
+            await new Promise(r => setTimeout(r, 10000));
+        }
+        console.log(`Vesting contract initialized`);
+    } else {
+        console.log(`Initializing vesting contract with all participants`);
+        await primeVestingContract.initializeVesting(participantsAddresses, vestingInfos, true);
+        console.log(`Vesting contract initialized`);
+    }
+
 };
 
 module.exports.tags = ["avalanche-prime-vesting"];
