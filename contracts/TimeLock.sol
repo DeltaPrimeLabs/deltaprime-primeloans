fcf2d7dad1779304418dbf0a5963dfaf70fa2be7
0xnaoki-san <naoki@deltaprime.io>
2024-07-01 17:48:54 +0900

    fix: new delay param check before update

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -36,6 +36,7 @@ contract Timelock {
 
     function setDelay(uint delay_) public {
         require(msg.sender == address(this), "Timelock::setDelay: Call must come from Timelock.");
+        require(delay_ >= delay, "Timelock::setDelay: New Delay must be higher than current delay.");
         require(delay_ >= MINIMUM_DELAY, "Timelock::setDelay: Delay must exceed minimum delay.");
         require(delay_ <= MAXIMUM_DELAY, "Timelock::setDelay: Delay must not exceed maximum delay.");
         delay = delay_;
