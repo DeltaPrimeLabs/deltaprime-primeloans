ec5d24304bfea1864112b9d1d9e1148c01a0d9ea
kavsky02 <kamilovsky@deltaprime.io>
2024-07-15 16:07:24 +0200

    Refactor bridging script to bridge arbitrum -> avalanche

--- tools/scripts/bridge-token.js
+++ tools/scripts/bridge-token.js
@@ -30,7 +30,7 @@ const IERC20ApproveAbi = [
 ];
 
 function initWallet(networkName) {
-    const key = fs.readFileSync(`./.secrets/${networkName}/admin`).toString().trim();
+    const key = fs.readFileSync(`./.secrets/${networkName}/deployer`).toString().trim();
     console.log(getUrlForNetwork(networkName));
     const provider = new ethers.providers.JsonRpcProvider(getUrlForNetwork(networkName));
 
@@ -40,17 +40,17 @@ function initWallet(networkName) {
 async function bridgeTokens(networkName) {
     const wallet = initWallet(networkName);
 
-    const recipientAddress = '0x15066d6c882e63b33e12179DE4FceCdfCa93De1d'
-    const primeTokenAddress = '0x33C8036E99082B0C395374832FECF70c42C7F298'
+    const recipientAddress = '0xbAc44698844f13cF0AF423b19040659b688ef036'
+    const primeTokenAddress = '0x3De81CE90f5A27C5E6A5aDb04b54ABA488a6d14E'
     const avalancheChainId = 106;
     const arbitrumChainId = 110;
-    const bridgeContractAddress = '0x35643752F4ea0ba70456F0CA1e2778f783206a20'
+    const bridgeContractAddress = '0x3De81CE90f5A27C5E6A5aDb04b54ABA488a6d14E'
 
     let defaultAdapterParams = ethers.utils.solidityPack(["uint16", "uint256"], [1, 200000])
 
     const receiverBytes32 = ethers.utils.defaultAbiCoder.encode(["address"], [recipientAddress])
-    const amountToSend =  toWei("10");
-    const destinationChain = arbitrumChainId;  // In case of Avalanche it should be 110
+    const amountToSend =  toWei("0.01");
+    const destinationChain = avalancheChainId;  // In case of Avalanche it should be 110
     const bridgeContract = new ethers.Contract(bridgeContractAddress, PrimeBridgeArtifact.abi, wallet);
     const primeTokenContract = new ethers.Contract(primeTokenAddress, IERC20ApproveAbi, wallet);
     await primeTokenContract.approve(bridgeContract.address, amountToSend);
@@ -59,7 +59,7 @@ async function bridgeTokens(networkName) {
 
     let nativeFee = (await bridgeContract.estimateSendFee(destinationChain, receiverBytes32, amountToSend, false, defaultAdapterParams)).nativeFee;
 
-    console.log(`Briding ${amountToSend} from Avalanche to Arbitrum to ${naokiAddress} with fee ${nativeFee}`);
+    console.log(`Briding ${amountToSend} from Arbitrum to Avalanche to ${recipientAddress} with fee ${nativeFee}`);
     await bridgeContract
         .sendFrom(
             wallet.address,
@@ -71,4 +71,4 @@ async function bridgeTokens(networkName) {
         );
 }
 
-bridgeTokens("avalanche");
+bridgeTokens("arbitrum");
