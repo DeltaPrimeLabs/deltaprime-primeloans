e7412ff48faffcaadec90ad9c55698e66c68a7f3
keizir <ultimatesofteng.work@gmail.com>
2024-07-15 23:19:06 +0800

    loan stats script jobs fix typo

--- lambda/loanStats/loanStatsArbJob.sh
+++ lambda/loanStats/loanStatsArbJob.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd /home/ubuntu/deltaprime-primeloans/lambda/loanStats && /home/ubuntu/.nvm/versions/node/v16.20.2/bin/node loanStatsArbJob.js
+cd /home/ubuntu/deltaprime-primeloans/lambda/loanStats && /home/ubuntu/.nvm/versions/node/v16.20.2/bin/node loanStatsArb.js
