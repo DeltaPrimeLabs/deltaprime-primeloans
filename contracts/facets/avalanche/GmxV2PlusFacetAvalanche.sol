cdd7894b30c3edf44909db362f20dc7fde0dab0c
kavsky02 <kamilovsky@deltaprime.io>
2024-07-18 14:32:32 +0200

    Add deployment files

--- contracts/facets/avalanche/GmxV2PlusFacetAvalanche.sol
+++ contracts/facets/avalanche/GmxV2PlusFacetAvalanche.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: 4b75b12e11d56665aead9bf0a142e92acef03154;
+// Last deployed from commit: c1391d85590ab530af967dbef6f857f87886a119;
 pragma solidity 0.8.17;
 
 //This path is updated during deployment
