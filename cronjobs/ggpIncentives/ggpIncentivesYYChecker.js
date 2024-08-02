2b5985ceb4b70ac7a35a1df4ba4a46b982d83cf0
keizir <ultimatesofteng.work@gmail.com>
2024-08-02 13:00:32 +0000

    ggp incentives yy wombat job

---
+++ cronjobs/ggpIncentives/ggpIncentivesYYChecker.js
@@ -0,0 +1,61 @@
+const fetch = require('node-fetch');
+const {
+  fetchAllDataFromDB
+} = require('../utils/helpers');
+const pingUrl = require('../.secrets/ping.json');
+
+const threshold = 0.000001;
+// 7.44047619 GGP per hour = 1250 GGP per week
+const expectedIncentives = 0.744047619; // change the value accordingly based on incentives of interval
+
+const ggpIncentivesChecker = async () => {
+  try {
+    const now = Math.floor(Date.now() / 1000);
+    const params = {
+      TableName: "ggp-incentives-yy-ava-prod",
+      FilterExpression: "#timestamp > :min",
+      ExpressionAttributeValues: {
+        ":min": now - 60 * 60
+      },
+      ExpressionAttributeNames: {
+        "#timestamp": "timestamp"
+      }
+    };
+
+    const result = await fetchAllDataFromDB(params, true);
+
+    let totalIncentivesPerHour = 0;
+
+    result.map((item) => {
+      totalIncentivesPerHour += item.ggpCollected ? Number(item.ggpCollected) : 0;
+    });
+
+    const multiplier = Math.round(totalIncentivesPerHour / expectedIncentives);
+
+    const diff = Math.abs(totalIncentivesPerHour / multiplier - expectedIncentives); 
+    console.log(`totalIncentivesPerHour: ${totalIncentivesPerHour / multiplier}, diff: ${diff}`);
+
+    if (diff < threshold) {
+      await fetch(pingUrl.ggpChecker.success);
+    } else {
+      await fetch(pingUrl.ggpChecker.fail, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          errorMessage: `difference higher than threshold`,
+          difference: diff,
+          threshold: threshold,
+          expectedIncentives,
+          sumOfIncentives: totalIncentivesPerHour / multiplier,
+          multiplier: multiplier
+        })
+      });
+    }
+  } catch(error) {
+    console.error('Error', error);
+  };
+};
+
+ggpIncentivesChecker();
