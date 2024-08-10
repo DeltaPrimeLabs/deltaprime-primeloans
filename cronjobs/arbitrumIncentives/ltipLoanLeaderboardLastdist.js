0f3e4ba9b62fb3160769ef5a51719810f8b76283
keizir <ultimatesofteng.work@gmail.com>
2024-08-10 12:28:09 +0000

    Update LTIP PA last dist timestamp 2

--- cronjobs/arbitrumIncentives/ltipLoanLeaderboardLastdist.js
+++ cronjobs/arbitrumIncentives/ltipLoanLeaderboardLastdist.js
@@ -4,7 +4,7 @@ const {
 } = require('../utils/helpers');
 
 const ltipLoanLeaderboard = async() => {
-  const from = 1722859200;
+  const from = 1721293803;
   const to = Math.floor(Date.now() / 1000);
 
   let params = {
