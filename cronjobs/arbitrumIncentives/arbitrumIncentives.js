f1b89012dc74df5f65b31cf587ab4a1bccd47d50
keizir <ultimatesofteng.work@gmail.com>
2024-08-12 12:47:05 +0000

    LTIP PA update incentives 50000 to 25000

--- cronjobs/arbitrumIncentives/arbitrumIncentives.js
+++ cronjobs/arbitrumIncentives/arbitrumIncentives.js
@@ -63,7 +63,7 @@ let retryTime = 0;
 
 const arbitrumIncentives = async (rpc = 'first') => {
   const now = Math.floor(Date.now() / 1000);
-  const incentivesPerWeek = 50000;
+  const incentivesPerWeek = 25000;
   const incentivesMultiplier = await getIncentivesMultiplier(now);
 
   if (incentivesMultiplier == 0) return;
