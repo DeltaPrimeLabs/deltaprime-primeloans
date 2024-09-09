c2f4bb76132e5a09c0552d02b985ab6e6e44063c
kavsky02 <kamilovsky@deltaprime.io>
2024-09-09 12:50:29 +0200

    Change public methods to external in Pool.sol, WrappedNativeTokenPool.sol

--- lambda/apis/ggpIncentives.js
+++ lambda/apis/ggpIncentives.js
@@ -68,6 +68,72 @@ const getGgpBoostApyApi = (event, context, callback) => {
     });
 };
 
+const getGgpIncentivesYYWombatForLastWeekApi = async (event, context, callback) => {
+    try {
+        const addresses = event.queryStringParameters.addresses.split(',');
+        const incentivesOfAddresses = {};
+
+        // Get current timestamp and one week ago timestamp
+        const now = Date.now();
+        const oneWeekAgo = (now - 7 * 24 * 60 * 60 * 1000) / 1000; // 1 week in seconds
+
+        await Promise.all(
+            addresses.map(async (address) => {
+                let currentParams = {
+                    TableName: process.env.GGP_INCENTIVES_YY_WOMBAT_AVA_TABLE,
+                    KeyConditionExpression: 'id = :paAddress',
+                    ExpressionAttributeValues: {
+                        ':paAddress': address.toLowerCase()
+                    }
+                };
+
+                // Fetch all current data
+                const currentIncentives = await fetchAllDataFromDB(currentParams, false);
+                let loanAccumulatedIncentivesNow = 0;
+                currentIncentives.map((item) => {
+                    loanAccumulatedIncentivesNow += item.ggpCollected ? Number(item.ggpCollected) : 0;
+                });
+
+                // Now let's fetch data from one week ago
+                let pastParams = {
+                    TableName: process.env.GGP_INCENTIVES_YY_WOMBAT_AVA_TABLE,
+                    KeyConditionExpression: 'id = :paAddress AND #ts <= :end',
+                    ExpressionAttributeNames: {
+                        '#ts': 'timestamp'
+                    },
+                    ExpressionAttributeValues: {
+                        ':paAddress': address.toLowerCase(),
+                        ':end': oneWeekAgo
+                    }
+                };
+
+                const pastIncentives = await fetchAllDataFromDB(pastParams, false);
+                let loanAccumulatedIncentivesOneWeekAgo = 0;
+                pastIncentives.map((item) => {
+                    loanAccumulatedIncentivesOneWeekAgo += item.ggpCollected ? Number(item.ggpCollected) : 0;
+                });
+
+                // Calculate the difference between now and one week ago
+                const ggpCollectedDifference = loanAccumulatedIncentivesNow - loanAccumulatedIncentivesOneWeekAgo;
+
+                incentivesOfAddresses[address] = {
+                    ggpCollectedDifference: ggpCollectedDifference,
+                    ggpCollectedNow: loanAccumulatedIncentivesNow,
+                    ggpCollectedOneWeekAgo: loanAccumulatedIncentivesOneWeekAgo
+                };
+            })
+        );
+
+        // Return the results
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(incentivesOfAddresses),
+        });
+    } catch (error) {
+        callback(error);
+    }
+};
+
 const getGgpIncentivesYYWombatForApi = async (event, context, callback) => {
   try {
     const addresses = event.queryStringParameters.addresses.split(',');
@@ -137,5 +203,6 @@ module.exports = {
   getGgpIncentivesForApi,
   getGgpBoostApyApi,
   getGgpIncentivesYYWombatForApi,
-  getGgpYYWombatBoostApyApi
+  getGgpYYWombatBoostApyApi,
+  getGgpIncentivesYYWombatForLastWeekApi
 }
