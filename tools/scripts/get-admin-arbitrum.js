ad3988df69131316878e0ea838e823c9a5f6bcaf
kavsky02 <kamilovsky@deltaprime.io>
2024-09-18 09:21:12 +0200

    Update script for owners/admins

--- tools/scripts/get-admin-arbitrum.js
+++ tools/scripts/get-admin-arbitrum.js
@@ -19,13 +19,43 @@ const poolMapping = {
     "UsdcPoolTUP": "0x8FE3842e0B7472a57f2A2D56cF6bCe08517A1De0",
     "WethPoolTUP": "0x0BeBEB5679115f143772CfD97359BBcc393d46b3",
     "UsdtPoolTUP": "0x5fAe0ebE49a920FA8350c0396683244824eECE74",
-    "ArbPoolTUP": "0x2B8C610F3fC6F883817637d15514293565C3d08A"
+    "ArbPoolTUP": "0x2B8C610F3fC6F883817637d15514293565C3d08A",
+    "DepositIndex BTC": "0x7789BFf818C051c48e7EBA3550D72AC9bE2fCAF7",
+    "DepositIndex DAI": "0x8894df0686b38d85d431F52660107b6F57609bC8",
+    "DepositIndex USDC": "0x56F67141ba2197854D447128A8Aad9B517b8fD43",
+    "DepositIndex WETH": "0x5C671Dae4C78f8beBa83aE67cee0f00B783E497C",
+    "DepositIndex ARB": "0xdEab10Eb04525Ad8eb466d7A3634a4fB27040147",
+    "BorrowIndex BTC": "0xD04E075E56194CFf489ec4941eF1FCb2BAB785AE",
+    "BorrowIndex DAI": "0xF5aD6c5aa8d5a05F22B730743949Bf1Ff5ec8c48",
+    "BorrowIndex USDC": "0xF5aD6c5aa8d5a05F22B730743949Bf1Ff5ec8c48",
+    "BorrowIndex WETH": "0x9d0Ac60fDF8BF94EaFF4489ACCFff4c8F5CD51C1",
+    "BorrowIndex ARB": "0xA508C3ceDb25aF7fBC3B9C339Fad6380470AFb74",
+    "Prime_L2": "0x3De81CE90f5A27C5E6A5aDb04b54ABA488a6d14E",
+    "sPrimeUniswapUP": "0x04d36A9aAD2072C69E4B0Cb2A403D8a893064945",
+    "vPrimeUP": "0x88fBaEa44b85fcC505c1aB1fD884c877A3b3dD42",
+    "vPrimeControllerUP": "0xA0Ff5eA5fB3A7739791b81c83A8742044893CFaC",
+    "SushiSwapIntermediary": "0xfd5665022359586Af38836b88E52E4690a3a7B79",
+    "BTC Rates calculator": "0x7Cb9b83c91482C88aC2A3Cc1CB599872F10830E6",
+    "BTC Rates calculator NEW": "0x290EBE564Fb778003f6D06cE6139848d3D2Aa14A",
+    "DAI Rates calculator": "0x7Eef8C4190E4ef1dCF5071c70afC612AD2a49956",
+    "DAI Rates calculator NEW": "0xd480d93BE366cd8fd4FC5EF0Df676F83944b5eBC",
+    "USDC Rates calculator": "0x6715e7768b6a9C523046407E0354a21bB17acb39",
+    "USDC Rates calculator NEW": "0xA24E8B383B1D8da473426768D5A85DB1Ae391DCe",
+    "WETH Rates calculator": "0x0D184ADF34065598311233A1d37765f76fbC5a72",
+    "WETH Rates calculator NEW": "0x02A4B9e27911513CFe62E5763f6A37f577c5AE5f",
+    "ARB Rates calculator": "0x5d776f18289C1CaDbb11D77723c3D0605912b34f",
+    "ARB Rates calculator NEW": "0xD4a3606A8b3e7b5F9e95C51500452a4c532Cfc45",
 };
 
 const knownAddresses = {
     "0xDfA6706FC583b635CD6daF0E3915901A2fBaBAaD": "MULTISIG OWNER",
     "0xa9Ca8462aB2949ADa86297904e09Ab4Eb12cdCf0": "MUTLISIG ADMIN",
+    "0x1fa4DeD27b8d395BF16f76a1c633B7D33CfF15E7": "TIMELOCK ADMIN",
+    "0x19a2e808BB716E20B7ceA28bc11930EF89B841ba": "TIMELOCK OWNER",
     "0x43D9A211BDdC5a925fA2b19910D44C51D5c9aa93": "Timelock24h",
+    "0x8f430e5d18CCa67288c74c72bb3326F62cc1f7B7": "LEDGER Kamil1",
+    "0x073b893284303708C515f4d246eE2f81e58d0ac4": "LEDGER Kamil3",
+
 };
 
 function getReadableName(address) {
@@ -47,8 +77,8 @@ async function getInfo(contractAddress) {
 async function main() {
     let data = [];
 
-    let { admin: diamondAdmin, owner: diamondOwner } = await getInfo(diamondAddress);
-    data.push({ Contract: "Diamond", Admin: `${diamondAdmin} (${getReadableName(diamondAdmin)})`, Owner: `${diamondOwner} (${getReadableName(diamondOwner)})` });
+    // let { admin: diamondAdmin, owner: diamondOwner } = await getInfo(diamondAddress);
+    // data.push({ Contract: "Diamond", Admin: `${diamondAdmin} (${getReadableName(diamondAdmin)})`, Owner: `${diamondOwner} (${getReadableName(diamondOwner)})` });
 
     let { admin: tokenManagerAdmin, owner: tokenManagerOwner } = await getInfo(tokenManagerAddress);
     data.push({ Contract: "TokenManager", Admin: `${tokenManagerAdmin} (${getReadableName(tokenManagerAdmin)})`, Owner: `${tokenManagerOwner} (${getReadableName(tokenManagerOwner)})` });
