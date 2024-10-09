fb77fcba38e9055b68a10929ab159d93c85ce293
mbare0 <mbare@deltaprime.io>
2024-10-09 22:30:38 +0200

    UI: update protocol insurance

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@
           <ThemeToggle v-if="!isClaimPage" class="top-bar__theme-toggle"></ThemeToggle>
           <div v-if="isSavingsPage" class="protocol-insurance">
             <span>Reserve Fund:</span>
-            <span class="insurance-value">$2,340,000</span>
+            <span class="insurance-value">$800,000</span>
             <InfoIcon class="info__icon"
                       :tooltip="{content: 'Protocol Reserve Fund and Atomica insurance pools.', classes: 'info-tooltip'}"
                       :classes="'info-tooltip'"></InfoIcon>
