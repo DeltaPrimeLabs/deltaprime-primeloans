1416cdaf9586f2b1d2285cf5c3e4570b89b42de6
kavsky02 <kamilovsky@deltaprime.io>
2024-10-10 16:54:40 +0200

    Add rTKN deployment files

--- contracts/rToken.sol
+++ contracts/rToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Last deployed from commit: 9ca20ca3522705e40ae7ceaae5107ba7d8c43a3e;
+// Last deployed from commit: 2f8ed44459fc99825919d399c3e4350b61551b67;
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
@@ -12,10 +12,7 @@ contract RTKNDP is Initializable, ERC20Upgradeable, OwnableUpgradeable {
         __Ownable_init();
     }
 
-    function mintBatch(address[] memory recipients, uint256[] memory amounts) public onlyOwner {
-        require(recipients.length == amounts.length, "Arrays must have the same length");
-        for (uint256 i = 0; i < recipients.length; i++) {
-            _mint(recipients[i], amounts[i]);
-        }
+    constructor(){
+        _disableInitializers();
     }
 }
--- deploy/arbitrum/38_rTKN.js
+++ deploy/arbitrum/38_rTKN.js
@@ -10,43 +10,43 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const MULTISIG_OWNER = "0xDfA6706FC583b635CD6daF0E3915901A2fBaBAaD"
     const MULTISIG_ADMIN = "0xa9Ca8462aB2949ADa86297904e09Ab4Eb12cdCf0"
 
-    embedCommitHash("rToken", "./contracts");
-
-    let RTKNDP = await deploy("RTKNDP", {
-        from: deployer,
-        gasLimit: 100000000,
-        args: [],
-    });
-
-
-    console.log(
-        `RTKNDP implementation deployed at address: ${RTKNDP.address}`
-    );
+    // embedCommitHash("rToken", "./contracts");
+    //
+    // let RTKNDP = await deploy("RTKNDP", {
+    //     from: deployer,
+    //     gasLimit: 100000000,
+    //     args: [],
+    // });
+    //
+    //
+    // console.log(
+    //     `RTKNDP implementation deployed at address: ${RTKNDP.address}`
+    // );
 
     await verifyContract(hre,
         {
-            address: RTKNDP.address,
+            address: "0xF27f8B5F37671a32a45581F17f83cEE439dD2Af2",
             contract: `contracts/rToken.sol:RTKNDP`,
             constructorArguments: []
         });
     console.log(`Verified RTKNDP`);
 
-    let resultTUP = await deploy(`RTKNTUP`, {
-        contract: `contracts/proxies/tup/arbitrum/rTKNTUP.sol:RTKNTUP`,
-        from: deployer,
-        gasLimit: 50000000,
-        args: [RTKNDP.address, MULTISIG_ADMIN, []],
-    });
-
-    console.log(`rTKNTUP deployed at address: ${resultTUP.address}`);
-
-    await verifyContract(hre,
-        {
-            address: resultTUP.address,
-            contract: `contracts/proxies/tup/arbitrum/rTKNTUP.sol:RTKNTUP`,
-            constructorArguments: [RTKNDP.address, MULTISIG_ADMIN, []]
-        });
-    console.log(`Verified rTKNTUP.sol`)
+    // let resultTUP = await deploy(`RTKNTUP`, {
+    //     contract: `contracts/proxies/tup/arbitrum/rTKNTUP.sol:RTKNTUP`,
+    //     from: deployer,
+    //     gasLimit: 50000000,
+    //     args: [RTKNDP.address, MULTISIG_ADMIN, []],
+    // });
+    //
+    // console.log(`rTKNTUP deployed at address: ${resultTUP.address}`);
+    //
+    // await verifyContract(hre,
+    //     {
+    //         address: resultTUP.address,
+    //         contract: `contracts/proxies/tup/arbitrum/rTKNTUP.sol:RTKNTUP`,
+    //         constructorArguments: [RTKNDP.address, MULTISIG_ADMIN, []]
+    //     });
+    // console.log(`Verified rTKNTUP.sol`)
 };
 
 module.exports.tags = ["arbitrum-rTKN"];
