6cfbd1f9811f0b8013391a80fa9d886e46b26343
LeChiffre <lechiffre@deltaprime.io>
2024-10-14 19:31:09 +0200

    UI: warning about CAI to be dropped support for

--- src/components/AssetsTableRow.vue
+++ src/components/AssetsTableRow.vue
@@ -9,7 +9,7 @@
             {{ asset.symbol }}
             <img v-if="asset.droppingSupport && assetBalances[asset.symbol] > 0"
                  src="src/assets/icons/warning.svg"
-                 v-tooltip="{content: `We will drop support to this asset on ${ asset.debtCoverage > 0.1 ? '26.04.2024 12:00 CET' : 'Monday 22.04.2024 16:00 CET'}. Please withdraw or swap to another token.`, classes: 'info-tooltip long'}">
+                 v-tooltip="{content: `We will drop support to this asset soon. Please withdraw or swap to another token.`, classes: 'info-tooltip long'}">
             <img v-if="asset.warning"
                  src="src/assets/icons/warning.svg"
                  v-tooltip="{content: `${asset.warning}`, classes: 'info-tooltip long'}">
@@ -374,6 +374,7 @@ export default {
           this.asset.symbol === 'CAI' ? {
             key: 'CAI_MINT',
             name: 'Mint CAI',
+            disabled: config.ASSETS_CONFIG['CAI'].droppingSupport,
           } : null,
 
           this.asset.symbol === 'CAI' ? {
